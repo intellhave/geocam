4cc19cacad03de3774f3c08d47ba20e367d43b1b
henniges@email.arizona.edu <henniges@email.arizona.edu@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2008-06-20 22:03:50 +0000

    added comments and makeTriangulationFile

--- trunk/triangulationInputOutput.cpp
+++ trunk/triangulationInputOutput.cpp
@@ -1,8 +1,19 @@
+/**************************************************************
+File: Triangulation Input/Output
+Author: Alex Henniges, Tom Williams, Mitch Wilson
+Version: June 20, 2008
+***************************************************************
+The Triangulation Input/Output file holds the functions that handle
+the reading and writing of text files.
+**************************************************************/
 #include <cstdlib>
 #include <iostream>
 #include <vector>
 #include "simplex/simplex.h"
 #include <map>
+#include <vector>
+#include <algorithm>
+#include <set>
 #include <fstream>
 #include <string>
 #include "simplex/vertex.h"
@@ -44,7 +55,8 @@
  * every simplex must have three lines of integers. The current
  * maximum size for a list of local simplices is 24. Finally, note that 
  * not all vertices must be listed before declaring edges or faces and 
- * that they do not need to be in any sort of order or numbering scheme. 
+ * that they do not need to be in any sort of order but should be from
+ * 1 to n. 
  */
 
 bool readTriangulationFile(char* fileName) 
@@ -297,3 +309,226 @@ void writeTriangulationFile(char* newFileName)
      }
      
 }
+
+struct Pair
+{
+       int v1, v2;
+       int positionOf(vector<Pair>*);  
+       bool contains(int);
+       bool isInTriple(vector<int>*);
+};
+
+int Pair::positionOf(vector<Pair>* list)
+{
+     for (int i = 0; i < (*list).size(); i++)
+     {
+         Pair other = (*list)[i];
+         if ((this->v1 == other.v1 && this-> v2 == other.v2)||(this->v1 == other.v2 && this-> v2 == other.v1))
+         {
+                return i;    
+         }
+     }
+     return -1;
+}
+
+bool Pair::contains(int i)
+{
+     if (i == v1 || i == v2)
+     {
+           return true;
+     }
+     else
+         return false;
+}
+ 
+bool Pair::isInTriple(vector<int>* triple)
+{
+     int result = 0;
+     for(int i = 0; i < triple->size(); i++)
+     {
+          if(v1 == (*triple)[i])
+          {
+                result++;
+          }
+          if(v2 == (*triple)[i])
+          {
+                result++;
+          }
+     }
+     return result == 2;
+}
+ 
+void makeTriangulationFile (char* from, char* to) {
+
+  ifstream infile;
+  vector< vector<int> > f; 
+  infile.open(from);
+  // >> i/o operations here <<
+  char temp[50];
+  infile.getline(temp, 50 ,'=');
+  char ch;
+  infile >> ch;
+  while (ch != ']')
+        {
+            infile >> ch;
+            vector<int> face;
+            for (int n = 1; n <= 3; n++)
+            {
+                int element;
+                infile >> element;
+                face.push_back(element);
+                infile >> ch;
+            }
+            f.push_back(face);
+            infile >> ch;
+        }
+        
+  vector<Pair> list; 
+  for(int i = 0; i < f.size(); i++)
+  {
+     Pair p1 = {(f[i])[0], (f[i])[1]};
+     Pair p2 = {(f[i])[0], (f[i])[2]};
+     Pair p3 = {(f[i])[1], (f[i])[2]};
+     if (p1.positionOf(&list) == -1)
+     {
+         list.push_back(p1);
+     }
+     if (p2.positionOf(&list) == -1)
+     {
+         list.push_back(p2);
+     }
+     if (p3.positionOf(&list) == -1)
+     {
+         list.push_back(p3);
+     }
+  }     
+  
+  vector<int> v;
+  vector<int>::iterator it;
+  for (int i = 0; i < f.size(); i++)
+  {
+      for (int j = 0; j < 3; j++)
+      {
+          it = find(v.begin(), v.end(), (f[i])[j]);
+          if (it == v.end())
+          {
+                 v.push_back((f[i])[j]);
+          }
+      }
+  }
+   
+  ofstream outfile;
+  outfile.open(to, ios_base::trunc);
+  
+  // Vertices
+  for (int i = 0; i < v.size(); i++)
+  {
+      // name
+      outfile << "Vertex: " << v[i] << "\n";
+      set<int> localv; 
+      vector<int> localf; 
+      
+      for (int k = 0; k < f.size(); k++)
+      {      
+          it = find(f[k].begin(), f[k].end(), v[i]);
+          if (it != f[k].end())
+          {
+                 localf.push_back(k+1);
+                 for(int g = 0; g < 3; g++)
+                 {
+                       localv.insert((f[k])[g]);
+                 }
+          }        
+      }
+      localv.erase(v[i]);
+      set<int>::iterator notit;
+      set<int>::iterator end = localv.end();
+      end--;
+      // local vertices
+      for (notit = localv.begin(); notit != end; notit++)
+      {
+          outfile << *notit << " ";
+      }
+      outfile << *notit << "\n";    
+      // local edges
+      for (int j = 0; j < list.size(); j++)
+      {
+          if (list[j].contains(v[i]))
+          {
+             outfile << j + 1 << " ";
+          }
+             
+      } 
+      outfile << "\n";
+      // local faces
+      for (int i = 0; i < localf.size(); i++)
+      {
+          outfile << localf[i] << " ";
+      }
+      outfile << "\n";
+  }
+  
+  //Edges
+  for(int i = 0; i < list.size(); i++)
+  {
+          // name
+          outfile << "Edge: " << i + 1 << "\n";
+          Pair current = list[i];
+          // local vertices
+          outfile << current.v1 << " " << current.v2 << "\n";
+          // local edges
+          for(int j = 0; j < list.size(); j++)
+          {
+             if(j != i && 
+                (list[j].contains(current.v1) || list[j].contains(current.v2)))
+             {
+                 outfile << j + 1 << " "; 
+             }
+          }
+          outfile << "\n";
+          for(int j = 0; j < f.size(); j++)
+          {
+                  if(current.isInTriple(&(f[j])))
+                  {
+                      outfile << j + 1 << " ";
+                  }
+          }
+          outfile << "\n";
+  }
+  
+  //Faces
+  for(int i = 0; i < f.size(); i++)
+  {
+      // name
+      outfile << "Face: " << i + 1 << "\n";
+      vector<int> current = f[i];
+      // local vertices
+      for(int j = 0; j < current.size(); j++)
+      {
+              outfile << current[j] << " ";
+      }
+      outfile << "\n";
+      // local edges
+      for(int j = 0; j < list.size(); j++)
+      {
+              if((list[j]).isInTriple(&current))
+              {
+                 outfile << j + 1 << " ";
+              }
+      }
+      outfile << "\n";
+      for(int j = 0; j < f.size(); j++)
+      {
+         if( j != i)
+         {
+            vector<int> inter = listIntersection(&current, &(f[j]));
+            if(inter.size() == 2)
+            {
+               outfile << j + 1 << " ";
+            }
+         }
+      }
+      outfile << "\n";   
+  }
+  
+  infile.close(); }
