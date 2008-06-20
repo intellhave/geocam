950a8be04693fc1807603c2b9f481e76dffea8f4
henniges@email.arizona.edu <henniges@email.arizona.edu@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2008-06-20 22:03:57 +0000

    added comments and makeTriangulationFile

--- trunk/triangulationInputOutput.h
+++ trunk/triangulationInputOutput.h
@@ -1,3 +1,13 @@
+/**************************************************************
+File: Triangulation Input/Output
+Author: Alex Henniges, Tom Williams, Mitch Wilson
+Version: June 20, 2008
+***************************************************************
+The Triangulation Input/Output file holds the functions that handle
+the reading and writing of text files.
+**************************************************************/
 bool readTriangulationFile(char*);
 
 void writeTriangulationFile(char*);
+
+void makeTriangulationFile(char*, char*);
