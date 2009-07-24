350e8993f4103be8ba41861777991f69c5229640
jthomas@email.arizona.edu <jthomas@email.arizona.edu@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2009-07-24 19:37:49 +0000

    Cleaned up the pipeline code, now in directory src.

--- trunk/pipeline/3DTriangulation/3DTriangulationMorph.h
+++
@@ -1,8 +0,0 @@
-#include "Triangulation/triangulationmath.h"
-#include "Triangulation/smallMorphs.h"
-#include "addremove.h"
-
-void oneFourMove(Tetra);
-void twoThreeMove(Face);
-void threeTwoMove(Edge);
-void fourOneMove(Vertex);
--- trunk/pipeline/Pipelined_Newtons_Method.h
+++
@@ -1,9 +0,0 @@
-#ifndef PIPELINED_NEWTONS_METHOD_H
-#define PIPELINED_NEWTONS_METHOD_H
-
-int LinearEquationsSolving(int nDim, double* pfMatr, 
-			double* pfVect, double* pfSolution);
-
-void Newtons_Method( int numsteps );
-
-#endif /* PIPELINED_NEWTONS_METHOD_H */
