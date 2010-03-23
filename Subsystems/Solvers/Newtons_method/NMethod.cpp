38baeaefe6ba8d73b16ba99b118fcef81244f6b5
henniges@email.arizona.edu <henniges@email.arizona.edu@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2010-03-23 18:32:31 +0000

    Changed NewtonsMethod to use our Matrix class for hessians. Tested and it works.

--- Subsystems/Solvers/Newtons_method/NMethod.cpp
+++ Subsystems/Solvers/Newtons_method/NMethod.cpp
@@ -6,6 +6,7 @@
  *=========================================================================*/
 #include "NMethod.h"
 #include "utilities.h"
+#include "miscmath.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
@@ -69,7 +70,7 @@ double NewtonsMethod::step(double x_n[]) {
    // Set grad[i] = - grad[i] for all i = 1,...,nDim
    negateArray(grad);
    // Use the linear equations solver to fill out values for next[].
-   if(LinearEquationsSolver(hess, grad, next) == 1) {
+   if(LinearEquationsSolver(hess, grad, next, nDim) == 1) {
      printf("Error with Solver\n");
      return -1;
    }
@@ -119,7 +120,7 @@ double NewtonsMethod::step(double x_n[], int extremum) {
    negateArray(grad);
    
    // Use the linear equations solver to fill out values for next[].
-   if(LinearEquationsSolver(hess, grad, next) == 1) {
+   if(LinearEquationsSolver(hess, grad, next, nDim) == 1) {
      printf("Error with Solver\n");
      return -1;
    }
@@ -246,7 +247,7 @@ void NewtonsMethod::approxGradient(double vars[], double sol[]) {
      }
 }
 
-void NewtonsMethod::approxHessian(double vars[], double *sol[]) {
+void NewtonsMethod::approxHessian(double vars[], Matrix<double>& sol) {
      double val = 0;        // A running sum for the numerator
      double f_x = f(vars);  // Save f(vars)
      double temp;
@@ -316,15 +317,7 @@ void NewtonsMethod::printInfo(FILE* out) {
   }
   fprintf(out, ">\n");
 
-  fprintf(out, "Hess = \n");
-  for(int i = 0; i < nDim; i++) {
-     fprintf(out, "<");
-     for(int j = 0; j < nDim; j++) {
-          fprintf(out, "%f, ", hess[i][j]);
-     }
-     fprintf(out, ">\n");
-  }
-  fprintf(out, "\n");
+  hess.print(out);
 /*DEBUG*/
 
 
@@ -332,98 +325,6 @@ void NewtonsMethod::printInfo(FILE* out) {
      printFunc(out);
    }
 } 
-
-//==============================================================================
-// return 1 if system not solving
-// nDim - system dimension
-// pfMatr - matrix with coefficients
-// pfVect - vector with free members
-// pfSolution - vector with system solution
-// pfMatr becames trianglular after function call
-// pfVect changes after function call
-//
-// Developer: Henry Guennadi Levkin
-//
-//==============================================================================
-int NewtonsMethod::LinearEquationsSolver(double *pfMatr[], double* pfVect, double* pfSolution)
-{
-  double maxElem; // Used to determine row with max element.
-  double temp; // Just a temp variable for switching 
-
-  int i, j, k, m; // indexing variables
-
-
-  // For each column in matrix...
-  for(k=0; k<(nDim-1); k++) 
-  // Here k will act as both the starting row and the 
-  // current column for the sub-matrix.
-  {
-    // Find the row with the max element in column k 
-    // (ignoring rows above k already completed).
-    // NOTE: This is to lessen potential rounding errors.
-    maxElem = fabs( pfMatr[k][k] ); // First element
-    m = k; // m is the index of the currently found max row
-    for(i=k+1; i<nDim; i++) // Iterate through every row below k.
-    {
-      if(maxElem < fabs(pfMatr[i][k]) )
-      { // If this row has the new max element... set values.
-        maxElem = fabs(pfMatr[i][k]);
-        m = i;
-      }
-    }
-    
-    // Permute the kth row with the row with the max element (if necessary).
-    if(m != k)
-    {
-      for(i=k; i<nDim; i++)
-      {
-        temp               = pfMatr[k][i];
-        pfMatr[k][i] = pfMatr[m][i];
-        pfMatr[m][i] = temp;
-      }
-      // Also permute the vector.
-      temp = pfVect[k];
-      pfVect[k] = pfVect[m];
-      pfVect[m] = temp;
-    }
-
-    if( pfMatr[k][k] == 0.) return 1; // Matrix has a column of all 0s !!!
-
-    // Triangulate matrix by turning every value in column k below row k 
-    // into a 0 using row operations.
-    for(j=(k+1); j<nDim; j++) // j is each row below row k.
-    {
-      // The multiplicative value in the row operation.
-      temp = - pfMatr[j][k] / pfMatr[k][k];
-       // For each entry in row j to the right of column k
-      for(i=k; i<nDim; i++)
-      { // perform row operation
-        pfMatr[j][i] += temp*pfMatr[k][i];
-      }
-      // Also perform operation on the vector
-      pfVect[j] = pfVect[j] + temp*pfVect[k];
-    }
-  }
-
-  // For each row beginning with the bottom
-  for(k=(nDim-1); k>=0; k--)
-  { // Calculate the solution for variable k.
-    pfSolution[k] = pfVect[k];
-    // Adjust solution using variables already known.
-    for(i=(k+1); i<nDim; i++)
-    {
-      pfSolution[k] -= (pfMatr[k][i]*pfSolution[i]);
-    }
-    // If the solution is 0, don't bother dividing.
-    if(fabs(pfMatr[k][k]) < 0.000000001) {
-       pfSolution[k] = 0;
-    } else {
-      pfSolution[k] = pfSolution[k] / pfMatr[k][k];
-    }
-  }
-
-  return 0;
-}
 
 double NewtonsMethod::getGradientLength(double gradient[]) {
    double sum = 0;
