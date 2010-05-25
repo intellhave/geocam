7db6fbbb6a566d9e949fbc3364586d66ad4c6f72
henniges@email.arizona.edu <henniges@email.arizona.edu@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2010-05-25 20:05:03 +0000

    Working on developing Geoquants. Added TriPosition.

--- trunk/SKUNK/src/Geoquant.java
+++ trunk/SKUNK/src/Geoquant.java
@@ -1,4 +1,54 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 
 public abstract class Geoquant {
+  protected ArrayList<Geoquant> dependents;
+  protected double value;
+  private boolean valid;
+	
+  protected abstract void recalculate();
+  protected abstract void remove();
+	
+  private Geoquant() {
+    dependents = new ArrayList<Geoquant>();
+    valid = false;
+  }
+  
 
+  protected void addDependent(Geoquant q) {
+    dependents.add(q);
+  }
+  
+  protected void removeDependent(Geoquant q) {
+    dependents.remove(q);
+  }
+  
+  public double getValue() {
+    if(! valid){
+      recalculate();
+      valid = true;
+    }
+    return value;		
+  }
+  
+  public void setValue(double val) {
+    value = val; 
+    invalidate();
+    valid = true;
+  }
+  
+  private void invalidate() {
+    if(valid){ 
+      valid = false;
+      notifyDependents();
+    }
+  }
+  
+  private void notifyDependents() {
+    Iterator<Geoquant> it = dependents.iterator();
+    while(it.hasNext()) {
+      it.next().invalidate();
+    }
+  }
 }
---
+++ trunk/SKUNK/src/TriPosition.java
@@ -0,0 +1,43 @@
+import java.io.PrintStream;
+import java.util.Collections;
+import java.util.ArrayList;
+
+
+public class TriPosition implements Comparable<TriPosition>{
+  private ArrayList<Integer> pointIDs;
+  
+  public TriPosition(int... points) {
+    pointIDs = new ArrayList<Integer>();
+    for(int point : points) {
+      pointIDs.add(point);
+    }
+    Collections.sort(pointIDs);
+  }
+  
+  public void print(PrintStream out) {
+    out.printf("[ " );
+    for(int point : pointIDs) {
+      out.printf("%d ", point);
+    }
+    out.printf("]");
+  }
+
+  public int compareTo(TriPosition y) {
+    int xLength = this.pointIDs.size();
+    int yLength = y.pointIDs.size();
+    
+    if(xLength != yLength) {
+      return xLength - yLength;
+    }
+    for(int ii = 0; ii < xLength; ii++){
+      if(this.pointIDs.get(ii) != y.pointIDs.get(ii)){
+        return this.pointIDs.get(ii) - y.pointIDs.get(ii); 
+      }
+    }
+    return 0;
+  }
+  
+  public boolean equals(Object other) {
+    return compareTo((TriPosition) other) == 0;
+  }
+}
