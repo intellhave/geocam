a9aeb05b11c9dad5d61e6f177f9b31b4e20c9566
henniges@email.arizona.edu <henniges@email.arizona.edu@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2010-05-27 00:29:41 +0000

    2D Lutz reading works in TriangulationIO

--- trunk/SKUNK/src/tests/IOTest.java
+++ trunk/SKUNK/src/tests/IOTest.java
@@ -1,7 +1,10 @@
 package tests;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+import Geoquant.Radius;
 import InputOutput.TriangulationIO;
 import Triangulation.Edge;
 import Triangulation.Face;
@@ -12,15 +15,37 @@ import Triangulation.Vertex;
 public class IOTest {
 
   public static void main(String[] args) {
-    test3DReader();
+    test2DLutz();
+  }
+    
+  private static void testReplace() {
+    Scanner scanner = null;
+    String s;
+    try {
+      scanner = new Scanner(new File("Data/2DManifolds/LutzFormat/tetrahedron.txt"));
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    s = "";
+    while(scanner.hasNextLine()) {
+      s = s.concat(scanner.nextLine());
+    }
+    s = s.replaceAll("[^0-9],[^0-9]", "\n");
+    s = s.replaceAll(",", " ");
+    s = s.replaceAll("[^0-9 \n]", "");
+    System.out.println(s);
   }
   
   private static void test2DReader() {
-    TriangulationIO.read2DTriangulationFile("Data/2DManifolds/StandardFormat/tetrahedron.txt");
+    TriangulationIO.read2DTriangulationFile("Data/2DManifolds/StandardFormat/tetrahedron mod.txt");
     
     for(Vertex v : Triangulation.vertexTable.values()) {
       System.out.println(v + ":\n\t" +
           v.getLocalVertices() + "\n\t" + v.getLocalEdges() + "\n\t" + v.getLocalFaces());
+      if(v.getIndex() == 1) {
+        System.out.println("Radius = " + Radius.valueAt(v));
+      }
     }
     System.out.println();
     for(Edge e : Triangulation.edgeTable.values()) {
@@ -59,6 +84,24 @@ public class IOTest {
       System.out.println(t + ":\n\t" +
           t.getLocalVertices() + "\n\t" + t.getLocalEdges() + "\n\t" + t.getLocalFaces() +
           "\n\t" + t.getLocalTetras());
+    }
+  }
+  
+  private static void test2DLutz() {
+    TriangulationIO.read2DLutzFile("Data/2DManifolds/LutzFormat/tetrahedron.txt");
+    for(Vertex v : Triangulation.vertexTable.values()) {
+      System.out.println(v + ":\n\t" +
+          v.getLocalVertices() + "\n\t" + v.getLocalEdges() + "\n\t" + v.getLocalFaces());
+    }
+    System.out.println();
+    for(Edge e : Triangulation.edgeTable.values()) {
+      System.out.println(e + ":\n\t" +
+          e.getLocalVertices() + "\n\t" + e.getLocalEdges() + "\n\t" + e.getLocalFaces());
+    }
+    System.out.println();
+    for(Face f : Triangulation.faceTable.values()) {
+      System.out.println(f + ":\n\t" +
+          f.getLocalVertices() + "\n\t" + f.getLocalEdges() + "\n\t" + f.getLocalFaces());
     }
   }
 
