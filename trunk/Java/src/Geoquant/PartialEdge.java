ea8ad82125a59c9703d302e2567a0d7837727389
henniges@email.arizona.edu <henniges@email.arizona.edu@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2010-06-03 17:57:04 +0000

    Added partial forms to: 	Curvature3D 	DihedralAngle 	PartialEdge

--- trunk/Java/src/Geoquant/PartialEdge.java
+++ trunk/Java/src/Geoquant/PartialEdge.java
@@ -6,17 +6,20 @@ import Triangulation.*;
 public class PartialEdge extends Geoquant {
   // Index map
   private static HashMap<TriPosition, PartialEdge> Index = new HashMap<TriPosition, PartialEdge>();
-  
+  private HashMap<TriPosition, Partial> PartialIndex;
   // Needed geoquants
   private Radius ri;
   private Radius rj;
   private Alpha ai;
   private Alpha aj;
   private Length Lij;
+  private Edge e;
   
   public PartialEdge(Vertex vertex, Edge edge) {
     super();
+    PartialIndex = new HashMap<TriPosition, Partial>();
     
+    this.e = edge;
     ri = Radius.At(vertex);
     ai = Alpha.At(vertex);
     for (Vertex v : edge.getLocalVertices()) {
@@ -70,6 +73,61 @@ public class PartialEdge extends Geoquant {
   
   public static double valueAt(Vertex vertex, Edge edge) {
     return At(vertex, edge).getValue();
+  }
+  
+  public PartialEdge.Partial partialAt(Edge f) {
+    TriPosition T = new TriPosition(f.getSerialNumber());
+    Partial q = PartialIndex.get(T);
+    if(q == null) {
+      q = new Partial(f);
+      q.pos = T;
+      PartialIndex.put(T, q);
+    }
+    return q;
+  }
+  
+  public class Partial extends Geoquant {
+    private boolean equal;
+    private Eta eij;
+    
+    private Partial(Edge f) {
+      equal = e == f;
+      if(equal) {
+        ri.addDependent(this);
+        rj.addDependent(this);
+        ai.addDependent(this);
+        aj.addDependent(this);
+        eij = Eta.At(e);
+        eij.addDependent(this);
+      }
+    }
+    
+    protected void recalculate() {
+      value = 0;
+      if(equal) {
+        double radi = ri.getValue();
+        double radj = rj.getValue();
+        double alphi = ai.getValue();
+        double alphj = aj.getValue();
+        double eta = eij.getValue();
+
+        value = radi * Math.pow(radj, 2) * (eta * radi + alphj * radj) 
+           / Math.pow(alphi * Math.pow(radi, 2) + alphj * Math.pow(radj, 2) + 2*eta*radi*radj, 1.5);
+      }
+    }
+
+    protected void remove() {
+      deleteDependents();
+      if(equal) {
+        ri.removeDependent(this);
+        rj.removeDependent(this);
+        ai.removeDependent(this);
+        aj.removeDependent(this);
+        eij.removeDependent(this);
+      }
+      PartialIndex.remove(pos);
+    }
+    
   }
 
 }
