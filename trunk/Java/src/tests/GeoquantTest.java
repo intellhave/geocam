806a20ccc913e5a95ac10aa6f88d432144196cfe
taya2j <taya2j@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2010-06-04 17:54:50 +0000

    Added geoquant tests.

--- trunk/Java/src/tests/GeoquantTest.java
+++ trunk/Java/src/tests/GeoquantTest.java
@@ -49,6 +49,11 @@ public class GeoquantTest {
     testPartialEdge();
     testNEHR();
     testVolumePartial();
+    testVolumeSecondPartial();
+    testDihedralAnglePartial();
+    testDihedralAngleSecondPartial();
+    testCurvature3DPartial();
+    testCurvature3DSecondPartial();
   }
   
   private static void testInvoker() {
@@ -95,6 +100,19 @@ public class GeoquantTest {
     }
     for (Edge edge : Triangulation.edgeTable.values()){
       Eta.At(edge).setValue(1.0);
+    }
+    for (Edge f : Triangulation.edgeTable.values()) {
+      Eta.At(f).setValue(1.0);
+    }
+    for (Vertex w : Triangulation.vertexTable.values()) {
+      Radius.At(w).setValue(1.0);
+      Alpha.At(w).setValue(1.0);
+    }
+    for (Edge nm : Triangulation.edgeTable.values()) {
+      Eta.At(nm).setValue(1.0);
+    }
+    for (Edge op : Triangulation.edgeTable.values()) {
+      Eta.At(op).setValue(1.0);
     }
   }
   
@@ -236,6 +254,85 @@ public class GeoquantTest {
       for (Edge e : t.getLocalEdges()){
         partial = vol.partialAt(e);
         System.out.println(partial + " at " + t + ", " + e + " = " + partial.getValue());
+      }
+    }
+  }
+  private static void testVolumeSecondPartial() {
+    Volume vol;
+    Volume.SecondPartial secondPartial;
+    for (Tetra t : Triangulation.tetraTable.values()){
+      vol = Volume.At(t);
+      for (Vertex v : t.getLocalVertices()){
+        for (Vertex w : t.getLocalVertices()){
+          secondPartial = vol.secondPartialAt(v, w);
+          System.out.println(secondPartial + " at " + v + ", " + w + " = " + secondPartial.getValue());
+        }
+        for (Edge e : t.getLocalEdges()){
+          secondPartial = vol.secondPartialAt(v, e);
+          System.out.println(secondPartial + " at " + v + ", " + e + " = " + secondPartial.getValue());
+        }
+      }
+      for (Edge e : t.getLocalEdges()){
+        for (Edge f : t.getLocalEdges()){
+          secondPartial = vol.secondPartialAt(e, f);
+          System.out.println(secondPartial + " at " + e + ", " + f + " = " + secondPartial.getValue());
+        }
+      }
+    }
+  }
+  private static void testDihedralAnglePartial() {
+    DihedralAngle d;
+    DihedralAngle.Partial partial;
+    for (Tetra t : Triangulation.tetraTable.values()){
+      for (Edge e : t.getLocalEdges()) {
+        d = DihedralAngle.At(e, t);
+        for (Edge f : t.getLocalEdges()) {
+          partial = d.partialAt(f);
+          System.out.println(partial + " at " + f + " = " + d.getValue());
+       }
+     }
+   }
+  }
+  private static void testDihedralAngleSecondPartial() {
+    DihedralAngle d;
+    DihedralAngle.SecondPartial secondPartial;
+    for (Tetra t : Triangulation.tetraTable.values()){
+      for (Edge e : t.getLocalEdges()) {
+        d = DihedralAngle.At(e, t);
+        for (Edge nm : t.getLocalEdges()) {
+          for (Edge op : t.getLocalEdges()) {
+            secondPartial = d.secondPartialAt(nm, op);
+            System.out.println(secondPartial + " at " + nm + ", " + op + " = " + d.getValue());
+          }
+        }
+      }
+    }
+  }
+  private static void testCurvature3DPartial() {
+    Curvature3D c;
+    Curvature3D.Partial partial;
+    for (Vertex v : Triangulation.vertexTable.values()){
+      c = Curvature3D.At(v);
+      for (Vertex w : Triangulation.vertexTable.values()) {
+        partial = c.partialAt(w);
+        System.out.println(partial + " at " + w + " = " + c.getValue());
+      }
+      for (Edge e : Triangulation.edgeTable.values()) {
+        partial = c.partialAt(e);
+        System.out.println(partial + " at " + e + " = " + c.getValue());
+      }
+    }
+  }
+  private static void testCurvature3DSecondPartial() {
+    Curvature3D c;
+    Curvature3D.SecondPartial secondPartial;
+    for (Vertex v : Triangulation.vertexTable.values()) {
+      c = Curvature3D.At(v);
+      for (Edge e : Triangulation.edgeTable.values()) {
+        for (Edge f : Triangulation.edgeTable.values()) {
+          secondPartial = c.secondPartialAt(e, f);
+          System.out.println(secondPartial + " at " + e + ", " + f + " = " + c.getValue());
+        }
       }
     }
   }
