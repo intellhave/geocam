e36e919674ce0aebc87d0f639ea2633f2ab1d339
kortox <kortox@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2010-06-08 20:36:55 +0000

    Seems to work for a fairly big triangulation (8 faces)

--- Java/src/Visualization/PlanarDevelopment.java
+++ Java/src/Visualization/PlanarDevelopment.java
@@ -5,11 +5,14 @@ import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.LinkedList;
 
+import Geoquant.Angle;
+import Geoquant.Curvature2D;
 import Geoquant.Length;
 import Triangulation.Edge;
 import Triangulation.Face;
 import Triangulation.Triangulation;
 import Triangulation.Vertex;
+import Triangulation.StdFace;
 
 public class PlanarDevelopment {
 
@@ -38,72 +41,166 @@ public class PlanarDevelopment {
     if (Triangulation.faceTable.isEmpty()) {
       return;
     } else {
+      StdFace.generateOrientation();
 
-      // perform a breadth first search of the vertices
-      LinkedList<Vertex> vertsToMap = new LinkedList<Vertex>();
-      HashSet<Vertex> vertsAlreadyMapped = new HashSet<Vertex>();
+      Face currFace = null;
+      // first face will be handled in a special way cause there aren't
+      // any faces already in the triangulation
+      currFace = Triangulation.faceTable.values().iterator().next();
+      StdFace stdFace = StdFace.getOrientedFace(currFace);
+      Point p1, p2, p3;
+      p1 = new Point(0, 0);
+      p2 = new Point(Length.valueAt(stdFace.e12), 0);
+      p3 = (new Point(Length.valueAt(stdFace.e13), 0)).rotate(Angle.valueAt(
+          stdFace.v1, currFace));
 
-      vertsToMap.addLast(Triangulation.vertexTable.values().iterator().next());
+      Line l12, l23, l13;
+      l12 = new Line(p1, p2);
+      l23 = new Line(p2, p3);
+      l13 = new Line(p1, p3);
 
-      while (!vertsToMap.isEmpty()) {
-        // we know this will succeed because the looping condition
-        Vertex vertex = vertsToMap.getFirst();
-        
-        //this will happen everytime after the first vertex
-        if (points.containsKey(vertex)) {
-          
-        } else { //the first vertex will be handled here
-          Edge startEdge = vertex.getLocalEdges().iterator().next();
+      points.put(stdFace.v1, p1);
+      points.put(stdFace.v2, p2);
+      points.put(stdFace.v3, p3);
+
+      lines.put(stdFace.e12, l12);
+      lines.put(stdFace.e23, l23);
+      lines.put(stdFace.e13, l13);
+      
+      System.err.println("Put Vertex " + stdFace.v1.getIndex() + " at point " + p1.toString());
+      System.err.println("Put Vertex " + stdFace.v2.getIndex() + " at point " + p2.toString());
+      System.err.println("Put Vertex " + stdFace.v3.getIndex() + " at point " + p3.toString());
+      
+      System.err.println("Put Edge " + stdFace.e12.getIndex() + " at points " + l12.toString());
+      System.err.println("Put Edge " + stdFace.e23.getIndex() + " at points " + l23.toString());
+      System.err.println("Put Edge " + stdFace.e13.getIndex() + " at points " + l13.toString());
+
+      HashSet<Face> alreadyMapped = new HashSet<Face>();
+      LinkedList<Face> facesToMap = new LinkedList<Face>();
+      
+      alreadyMapped.add(currFace);
+      for (Face f : currFace.getLocalFaces()) {
+        if (!alreadyMapped.contains(f)) {
+          facesToMap.addLast(f);
+        }
+      }
+      
+      while (!facesToMap.isEmpty()) {
+        currFace = facesToMap.removeFirst();
+        System.err.println("Working on face " + currFace.getIndex());
+        //might as well prep for the next iteration here so we don't forget
+        alreadyMapped.add(currFace);
+        for (Face f : currFace.getLocalFaces()) {
+          if (!alreadyMapped.contains(f)) {
+            facesToMap.addLast(f);
+          }
         }
         
+        stdFace = StdFace.getOrientedFace(currFace);
 
-        // we don't want to map the same vertex twice
-        vertsAlreadyMapped.add(vertex);
-      }
+        p1 = new Point(0, 0);
+        p2 = new Point(Length.valueAt(stdFace.e12), 0);
+        p3 = (new Point(Length.valueAt(stdFace.e13), 0)).rotate(Angle.valueAt(stdFace.v1, currFace));
+
+        // find an edge that has already been mapped
+        Edge mappedEdge = null;
+        for (Edge e : currFace.getLocalEdges()) {
+          if (lines.containsKey(e)) {
+            mappedEdge = e;
+            break;
+          }
+        }
+        if (mappedEdge == null) {
+          System.err.println("None of this face's edges have been mapped");
+        }
+
+        if (stdFace.e12.equals(mappedEdge)) { // matches the edge along the
+                                              // x-axis
+          Point mappedP1, mappedP2;
+          mappedP1 = points.get(stdFace.v1);
+          mappedP2 = points.get(stdFace.v2);
+          if (mappedP1 == null) {
+            System.err.println("yo currFace " + currFace.getIndex());
+          }
+          double angleDiff = p2.subtract(p1).angle() - mappedP2.subtract(mappedP1).angle();
+
+          Point thirdPoint = null;
+          if (points.containsKey(stdFace.v3)) {
+            thirdPoint = points.get(stdFace.v3);
+          } else {
+            thirdPoint = p3.rotate(angleDiff).add(points.get(stdFace.v1));
+            points.put(stdFace.v3, thirdPoint);
+            System.err.println("Put Vertex " + stdFace.v3.getIndex() + " at point " + thirdPoint.toString());
+          }
+          if (!lines.containsKey(stdFace.e13)) {
+            lines.put(stdFace.e13, new Line(mappedP1, thirdPoint));
+          }
+          if (!lines.containsKey(stdFace.e23)) {
+            lines.put(stdFace.e23, new Line(mappedP2, thirdPoint));
+          }
+        } else if (stdFace.e23.equals(mappedEdge)) { // matches the edge far
+                                                     // from the origin
+          Point mappedP2, mappedP3;
+          mappedP2 = points.get(stdFace.v2);
+          mappedP3 = points.get(stdFace.v3);
+          double angleDiff = p3.subtract(p2).angle() - mappedP3.subtract(mappedP2).angle();
+
+          Point oddPoint = null;
+          if (points.containsKey(stdFace.v1)) {
+            oddPoint = points.get(stdFace.v1);
+          } else {
+            // this is tricky
+            oddPoint = p1.subtract(p2).rotate(angleDiff).add(points.get(stdFace.v2));
+            points.put(stdFace.v1, oddPoint);
+            System.err.println("Put Vertex " + stdFace.v1.getIndex() + " at point " + oddPoint.toString());
+          }
+          if (!lines.containsKey(stdFace.e13)) {
+            lines.put(stdFace.e13, new Line(oddPoint, mappedP3));
+          }
+          if (!lines.containsKey(stdFace.e12)) {
+            lines.put(stdFace.e12, new Line(oddPoint, mappedP2));
+          }
+        } else if (stdFace.e13.equals(mappedEdge)) { // matches the edge off the
+                                                     // x-axis
+          Point mappedP1, mappedP3;
+          mappedP1 = points.get(stdFace.v1);
+          mappedP3 = points.get(stdFace.v3);
+          double angleDiff = p3.subtract(p1).angle() - mappedP3.subtract(mappedP1).angle();
+
+          Point oddPoint = null;
+          if (points.containsKey(stdFace.v2)) {
+            oddPoint = points.get(stdFace.v2);
+          } else {
+            // this is tricky
+            oddPoint = p2.rotate(angleDiff).add(points.get(stdFace.v1));
+            points.put(stdFace.v2, oddPoint);
+            System.err.println("Put Vertex " + stdFace.v1.getIndex() + " at point " + oddPoint.toString());
+          }
+          if (!lines.containsKey(stdFace.e23)) {
+            lines.put(stdFace.e23, new Line(oddPoint, mappedP3));
+          }
+          if (!lines.containsKey(stdFace.e12)) {
+            lines.put(stdFace.e12, new Line(mappedP1, oddPoint));
+          }
+        }
+        //done
+      }//end of while loop
     }
   }
 
-  //mutates the edges and faces parameters
-  private static void generateCyclicOrderings(Vertex vertex, Edge startEdge,
-      Face startFace, LinkedList<Edge> edges, LinkedList<Face> faces) {
-    
-    HashSet<Face> visitedFaces = new HashSet<Face>();
-    HashSet<Edge> visitedEdges = new HashSet<Edge>();
-    Edge currEdge = startEdge;
-    Face currFace = startFace;
-    
-    while(!visitedEdges.contains(currEdge)) {
-      edges.addLast(currEdge);
-      faces.addLast(currFace);
-      
-      visitedEdges.add(currEdge);
-      visitedFaces.add(currFace);
-      
-      //find the other edge on this face/vertex
-      for (Edge edge : currFace.getLocalEdges()) {
-        if (vertex.isAdjEdge(edge) && !currEdge.equals(edge) ) {
-          currEdge = edge;
-          break;
-        }
-      }
-      
-      //find the next face
-      for (Face face : currEdge.getLocalFaces()) {
-        if (!visitedFaces.contains(face)) {
-          currFace = face;
-          break;
-        }
-      }
+  public void printDevelopment() {
+    for (Vertex v : points.keySet()) {
+      System.out.println("Vertex " + v.getIndex() + " at " + points.get(v));
     }
-    
-    //might want the first edge to appear again at the end
-    //not sure about this though TODO
-    edges.addLast(currEdge);
+    for (Edge e : lines.keySet()) {
+      System.out.println("Edge " + e.getIndex() + " at " + lines.get(e));
+    }
   }
 
-  private void developEdge(Edge currEdge, Edge prevEdge, Face face,
-      Vertex vertex) {
-
+  public void printForPython() {
+    for (Edge e : lines.keySet()) {
+      System.out.println("l.append(" + lines.get(e) + ")");
+    }
   }
 
   // public void generatePlaneHelper(LinkedList<Face> facesToMap, HashSet<Face>
