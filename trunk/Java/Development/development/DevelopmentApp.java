b5bfaee5ab5e3b1a30d8a9bb90f227bff464f5e5
tdm@math.arizona.edu <tdm@math.arizona.edu@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2010-07-09 00:09:29 +0000

    AffineTransformation

--- trunk/Java/Development/development/DevelopmentApp.java
+++ trunk/Java/Development/development/DevelopmentApp.java
@@ -1,20 +1,55 @@
+
 package development;
+
+import util.Matrix;
 
 public class DevelopmentApp {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args)  {
+  /**
+   * @param args
+   */
+  public static void main(String[] args)  {
 
-		Vector a = new Vector(new double[] {2,3,4});
-		Vector b = new Vector(1,0,0);
-		try{
-			a.add(b);
-			System.out.print(a);
-		}catch(Exception e){
-			System.err.print(e.getMessage());
-		}
-	}
+    /*Vector a = new Vector(new double[] {2,3,4});
+    Vector b = new Vector(1,0,0);
+    try{
+      a.add(b);
+      System.out.print(a);
+    }catch(Exception e){
+      System.err.print(e.getMessage());
+    }*/
+    
+    Point p = new Point(new double[] {1,1});
+    Vector v = new Vector(new double[] {1,1});
+    Vector c = new Vector(new double[] {0,1});
+    Matrix t = new Matrix(2,2);
+    t.setEntry(0,0,0);
+    t.setEntry(0,1,-1);
+    t.setEntry(1,0,1);
+    t.setEntry(1,1,0);
+    try{
+      //AffineTransformation s = new AffineTransformation(t,c);
+      //AffineTransformation s = new AffineTransformation(t);
+      AffineTransformation s = new AffineTransformation(c);
+      Point q = new Point(s.affineTransPoint(p));
+      //Vector q = new Vector(s.affineTransVector(v));
+      System.out.print(s);
+      //System.out.print(q);
+      System.out.print(q);
+    }catch(Exception e){
+      e.printStackTrace();
+    
+    
+    
+  
+  }
+  
+  
+  }
+  
+  
+  
 
 }
+
+  
