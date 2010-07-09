7c27b84d75bcd3839158b83499a9c7a7f372beba
snaffle.byte <snaffle.byte@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2010-07-09 23:59:08 +0000

    Finished Frustum3D

--- trunk/Java/Development/development/Frustum3D.java
+++ trunk/Java/Development/development/Frustum3D.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Frustum3D {
 
-  private static final double epsilon = 10 ^ (-6);
+  private static final double epsilon = Math.pow(10, -6);
   ArrayList<Vector3D> vectors = new ArrayList<Vector3D>();
   ArrayList<Vector3D> normals = new ArrayList<Vector3D>();
 
@@ -17,12 +17,12 @@ public class Frustum3D {
     }
     findNormals();
   }
-  
+
   public Frustum3D(ArrayList<Vector3D> input) {
     vectors = input;
     findNormals();
   }
-  
+
   public int getNumberVectors() {
     return vectors.size();
   }
@@ -66,67 +66,130 @@ public class Frustum3D {
     }
     return true;
   }
-  
+
   public int findOutsideVectorIndex(Frustum3D frustum) throws Exception {
     ArrayList<Vector3D> vectors = frustum.getVectors();
-    for(int i = 0; i < vectors.size(); i++) {
-      if(!this.checkInterior(vectors.get(i))) return i;
+    for (int i = 0; i < vectors.size(); i++) {
+      if (!this.checkInterior(vectors.get(i)))
+        return i;
     }
     return -1;
   }
-  
+
   public Vector getVectorAt(int index) {
     return vectors.get(index);
   }
 
   public static Frustum3D intersect(Frustum3D frustum1, Frustum3D frustum2)
       throws Exception {
-    
-    if(frustum1.findOutsideVectorIndex(frustum2) == -1) return frustum2;
+
+    System.out.println();
+    System.out.println();
+    if (frustum1.findOutsideVectorIndex(frustum2) == -1)
+      return frustum2;
     int outsideIndex = frustum2.findOutsideVectorIndex(frustum1);
-    if (outsideIndex == -1) return frustum1;    
-    
+    if (outsideIndex == -1)
+      return frustum1;
+
+    System.out.println("vertex index " + outsideIndex
+        + " in frustum2 is outside frustum 1");
+
     ArrayList<Vector3D> newVectors = new ArrayList<Vector3D>();
     ArrayList<Vector3D> normals1 = frustum1.getNormals();
     ArrayList<Vector3D> normals2 = frustum2.getNormals();
 
     ArrayList<Vector3D> vectors1 = frustum1.getVectors();
+    ArrayList<Vector3D> vectors2 = frustum2.getVectors();
 
-    for (int i = 0; i < vectors1.size() - 1; i++) {
-      if (frustum2.checkInterior(vectors1.get(i)))
+    for (int i = 0; i < vectors1.size(); i++) {
+      if (frustum2.checkInterior(vectors1.get(i))) {
         newVectors.add(vectors1.get(i));
+        System.out.println("adding vector from frustum1");
+      } else
+        System.out.println("vector " + i + " from 1 is not contained in 2");
 
       Vector3D norm = normals1.get(i);
 
       ArrayList<Vector3D> intersections = new ArrayList<Vector3D>();
-      int count = 0;
-      int j = outsideIndex;
-      while(count < normals2.size()) {
+      int lastIntersectionIndex = 0;
+      for (int j = 0; j < normals2.size(); j++) {
         Vector3D intersection = GeometryOperations.intersectPlanes(norm,
             normals2.get(j));
+        boolean intersects = false;
         // check if intersection or its negation is contained in both frustums
         if (frustum1.checkInterior(intersection)
-            && frustum2.checkInterior(intersection))
-          intersections.add(intersection);
-        else {
+            && frustum2.checkInterior(intersection)) {
+          intersects = true;
+          System.out.println("found intersection of " + i + " from 1 and " + j
+              + " from 2");
+        } else {
           intersection.scale(-1);
           if (frustum1.checkInterior(intersection)
-              && frustum2.checkInterior(intersection))
-            intersections.add(intersection);
+              && frustum2.checkInterior(intersection)) {
+            // intersections.add(intersection);
+            intersects = true;
+            System.out.println("found negation of intersection of " + i
+                + " from 1 and " + j + " from 2");
+          }
         }
-        if(intersections.size() == 2) break;
-        // should never have more than 2
-        j = (j+1)%normals2.size();
-        count++;
+        System.out.println("length = " + intersection.length());
+        if(intersection.length() < epsilon) intersects = false;
+
+        if (intersects) {
+          intersection.normalize();
+          if (!closeTogether(intersection, vectors1.get(i))
+              && !closeTogether(intersection, vectors1.get((i + 1)
+                  % vectors1.size()))
+              && !closeTogether(intersection, vectors2.get(j))
+              && !closeTogether(intersection, vectors2.get((j + 1)
+                  % vectors2.size()))) {
+            if (intersections.size() == 0) {
+              System.out.println("adding first");
+              intersections.add(intersection);
+              lastIntersectionIndex = j;
+            } else {
+              Vector difference = Vector.subtract(vectors1.get((i + 1)
+                  % vectors1.size()), vectors1.get(i % vectors1.size()));
+              double dot = Vector.dot(difference, normals2.get(j));
+              if (dot < 0) {
+                intersections.add(intersection);
+                lastIntersectionIndex = j;
+                System.out.println("adding at end");
+              } else if (dot > 0) {
+                intersections.add(0, intersection);
+                System.out.println("adding at beginning");
+              }
+              else System.out.println("not adding");
+            }
+          }
+        }
       }
-      if(intersections.size() == 1) newVectors.add(intersections.get(0));
-      else if(intersections.size() == 2) {
-        newVectors.add(intersections.get(1));
-        newVectors.add(intersections.get(0));
+      newVectors.addAll(intersections);
+      System.out.println("found " + intersections.size() + " intersections");
+      if (!intersections.isEmpty()) {
+        // check next vertex in 2
+        int k = (lastIntersectionIndex + 1) % vectors2.size();
+        while (frustum1.checkInterior(vectors2.get(k))
+            && !newVectors.contains(vectors2.get(k))) {
+          System.out.println("adding vector " + k + " from 2");
+          newVectors.add(vectors2.get(k));
+          k = (k + 1) % vectors2.size();
+        }
       }
     }
-    if(newVectors.size() == 0) return null;
+
+    System.out.println("found " + newVectors.size() + " vertices");
+    if (newVectors.size() < 3)
+      return null;
     return new Frustum3D(newVectors);
   }
 
+  public static boolean closeTogether(Vector3D v1, Vector3D v2)
+      throws Exception {
+    Vector difference = Vector.subtract(v1, v2);
+    double cos = (v1.lengthSquared() + v2.lengthSquared() - difference
+        .lengthSquared())
+        / (2 * v1.length() * v2.length());
+    return Math.abs(cos) > 1 - epsilon;
+  }
 }
