57dba4a3aca695d6b27f34a331ad6112de6cee97
tdm@math.arizona.edu <tdm@math.arizona.edu@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2010-07-13 20:46:11 +0000

    Updated coordtrans geoquants

--- trunk/Java/Development/development/CoordTrans3D.java
+++ trunk/Java/Development/development/CoordTrans3D.java
@@ -15,24 +15,31 @@ public class CoordTrans3D extends Geoquant {
 
   //the common face between tetras t1 and t2 has vertices v1, v2, v3
   //this gives matrix moving tetra t1 onto its common face with t2
-  private Coord3D cv1t1;
-  private Coord3D cv1t2;
-  private Coord3D cv2t1;
-  private Coord3D cv2t2;
-  private Coord3D cv3t1;
-  private Coord3D cv3t2;
+  //to get the orientation right, we need the non-common vertices, w1 and w2
+  private Coord3D cv1t1,cv2t1,cv3t1,cw1;
+  private Coord3D cv1t2,cv2t2,cv3t2,cw2;
 
   private AffineTransformation affineTrans; 
   
   public CoordTrans3D(Tetra t1, Tetra t2) {
     super(t1,t2);
+
+    //get list of common vertices
+    LinkedList<Vertex> vertscommon = new LinkedList<Vertex>(t1.getLocalVertices());
+    vertscommon.retainAll(t2.getLocalVertices());
+    Vertex v1 = vertscommon.get(0);
+    Vertex v2 = vertscommon.get(1);
+    Vertex v3 = vertscommon.get(2);
     
-    //find common verts, must have retlist.size()==3
-    LinkedList<Vertex> retlist = new LinkedList<Vertex>(t1.getLocalVertices());
-    retlist.retainAll(t2.getLocalVertices());
-    Vertex v1 = retlist.get(0);
-    Vertex v2 = retlist.get(1);
-    Vertex v3 = retlist.get(2);
+    //get non-common vertex on tetra 1
+    LinkedList<Vertex> leftover1 = new LinkedList<Vertex>(t1.getLocalVertices());
+    leftover1.removeAll(vertscommon);
+    Vertex w1 = leftover1.get(0);
+    
+    //get non-common vertex on tetra 2
+    LinkedList<Vertex> leftover2 = new LinkedList<Vertex>(t2.getLocalVertices());
+    leftover2.removeAll(vertscommon);
+    Vertex w2 = leftover2.get(0);
     
     cv1t1 = Coord3D.At(v1, t1);
     cv2t1 = Coord3D.At(v2, t1);
@@ -40,6 +47,8 @@ public class CoordTrans3D extends Geoquant {
     cv1t2 = Coord3D.At(v1, t2);
     cv2t2 = Coord3D.At(v2, t2);
     cv3t2 = Coord3D.At(v3, t2);
+    cw1 = Coord3D.At(w1, t1);
+    cw2 = Coord3D.At(w2, t2);
     
     cv1t1.addObserver(this);
     cv2t1.addObserver(this);
@@ -47,6 +56,8 @@ public class CoordTrans3D extends Geoquant {
     cv1t2.addObserver(this);
     cv2t2.addObserver(this);
     cv3t2.addObserver(this);
+    cw1.addObserver(this);
+    cw2.addObserver(this);
   }
   
   protected void recalculate() {
@@ -66,6 +77,8 @@ public class CoordTrans3D extends Geoquant {
     cv1t2.deleteObserver(this);
     cv2t2.deleteObserver(this);
     cv3t2.deleteObserver(this);
+    cw1.deleteObserver(this);
+    cw2.deleteObserver(this);
     Index.remove(pos);
   }
   
