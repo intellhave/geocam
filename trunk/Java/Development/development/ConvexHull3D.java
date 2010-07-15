367c380c73912649cd4cafccf2759e7104b19256
snaffle.byte <snaffle.byte@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2010-07-15 23:51:14 +0000

    Finished ConvexHull3D?!

--- trunk/Java/Development/development/ConvexHull3D.java
+++ trunk/Java/Development/development/ConvexHull3D.java
@@ -7,10 +7,16 @@ public class ConvexHull3D {
   private static final double epsilon = Math.pow(10, -6);
   private ArrayList<Face> faces = new ArrayList<Face>();
   private ArrayList<Vector3D> unsorted;
+  private ArrayList<Face> visible = new ArrayList<Face>();
+  private ArrayList<Face> hiddenFaces = new ArrayList<Face>();
+  private ArrayList<Face> newF = new ArrayList<Face>();
+  private ArrayList<Vector3D> vertices = new ArrayList<Vector3D>();
+  private Vector3D lastPoint = null;
 
   public ConvexHull3D(ArrayList<Vector3D> unsortedVectors) {
     unsorted = unsortedVectors;
     findHull(unsorted);
+    lastPoint = unsorted.get(0);
   }
 
   public ConvexHull3D(Vector3D... vectors) {
@@ -43,6 +49,38 @@ public class ConvexHull3D {
   
   public Face getFaceAt(int i) {
     return faces.get(i);
+  }
+  
+  public ArrayList<Face> getFaces() {
+    ArrayList<Face> list = new ArrayList<Face>();
+    for(int i = 0; i < faces.size(); i++)
+      list.add(new Face(faces.get(i)));
+    return list;
+  }
+  
+  // TODO for testing
+  public Vector3D lastPoint() {
+    return lastPoint;
+  }
+
+  public ArrayList<Face> getHiddenFaces() {
+    return hiddenFaces;
+  }
+  
+  public ArrayList<Face> getVisibleFaces() {
+    return visible;
+  }
+  
+  public ArrayList<Face> getNewFaces() {
+    return newF;
+  }
+  
+  public ArrayList<Vector3D> getVertices() {
+    ArrayList<Vector3D> newVertices = new ArrayList<Vector3D>();
+    for(int i = 0; i < vertices.size(); i++) {
+     newVertices.add(new Vector3D(vertices.get(i))); 
+    }
+    return newVertices;
   }
 
   /*
@@ -111,6 +149,9 @@ public class ConvexHull3D {
    * Removes the visible faces
    */
   private void addPoint(Vector3D v) {
+    lastPoint = v;
+    visible.clear();
+    hiddenFaces.clear();
     System.out.println("adding point: " + v);
     ArrayList<Face> visibleFaces = new ArrayList<Face>();
     ArrayList<Face> coplanarFaces = new ArrayList<Face>();
@@ -119,16 +160,22 @@ public class ConvexHull3D {
       double dot = Vector3D.dot(faces.get(i).getNormal(), Vector3D.subtract(v,
           faces.get(i).getVectorAt(0)));
       if (Math.abs(dot) < epsilon) { // call it planar
+        System.out.println("coplanar with face: ");
+        for(int j = 0; j < faces.get(i).getNumberVertices(); j++)
+          System.out.println(faces.get(i).getVectorAt(j));
+        System.out.println();
         coplanarFaces.add(faces.get(i));
-
+        hiddenFaces.add(faces.get(i));  // TODO testing
       } else if (dot >= epsilon) {
         visibleFaces.add(faces.get(i));
-      } // else not visible
+        visible.add(faces.get(i)); // TODO testing
+      } else // else not visible
+        hiddenFaces.add(faces.get(i));  // TODO testing
     }
 
     ArrayList<HashSet<Vector3D>> badEdges = getBadEdges(visibleFaces);
 
-    ArrayList<Vector3D> vertices = findAttachmentLoop(visibleFaces, badEdges);
+    vertices = findAttachmentLoop(visibleFaces, badEdges);
     System.out.println("vertices: " );
     for(int i = 0; i < vertices.size(); i++) {
       System.out.println(vertices.get(i));
@@ -226,6 +273,17 @@ public class ConvexHull3D {
    * between the last and first added from newFace.
    */
   private void mergeFaces(Face oldFace, Face newFace) {
+    System.out.println("merging faces: ");
+    System.out.println("oldFace");
+    for(int i = 0; i < oldFace.getNumberVertices(); i++) {
+      System.out.println(oldFace.getVectorAt(i));
+    }
+    System.out.println();
+    System.out.println("newFace");
+    for(int i = 0; i < newFace.getNumberVertices(); i++) {
+      System.out.println(newFace.getVectorAt(i));
+    }
+    System.out.println();
     ArrayList<Vector3D> vectors = new ArrayList<Vector3D>();
 
     int startIndex = 0; // will be index of vector before the non-shared one.
@@ -238,24 +296,40 @@ public class ConvexHull3D {
       startIndex = 2;
 
     // add vectors from newFace
-    vectors.add(newFace.getVectorAt(startIndex));
-    vectors.add(newFace.getVectorAt((startIndex + 1)
-        % newFace.getNumberVertices()));
-    vectors.add(newFace.getVectorAt((startIndex + 2)
-        % newFace.getNumberVertices()));
-
-    Vector3D v = newFace.getVectorAt((startIndex + 2)
+    System.out.println("on new face");
+    Vector3D v1 = newFace.getVectorAt(startIndex);
+    Vector3D v2 = newFace.getVectorAt((startIndex + 1)
         % newFace.getNumberVertices());
+    Vector3D v3 = newFace.getVectorAt((startIndex + 2)
+        % newFace.getNumberVertices());
+    System.out.println("adding " + v1);
+    System.out.println("adding " + v2);
+    System.out.println("adding " + v3);
+    vectors.add(v1);
+    vectors.add(v2);
+    vectors.add(v3);
 
     // add vectors from oldFace
+    System.out.println("on old face");
     boolean done = false;
-    for (int i = (oldFace.indexOf(v) + 1) % oldFace.getNumberVertices(); !done; i++) {
+    for (int i = (oldFace.indexOf(v3) + 1) % oldFace.getNumberVertices(); !done; i++) {
       int index = i % oldFace.getNumberVertices();
-      if (oldFace.getVectorAt(index).equals(v))
+      System.out.println("at " + oldFace.getVectorAt(index));
+      if (oldFace.getVectorAt(index).equals(v1)) {
         done = true;
-      else
+        System.out.println("not adding");
+      }
+      else {
         vectors.add(oldFace.getVectorAt(index));
+        System.out.println("adding");
+      }
     }
+    
+    System.out.println("result");
+    for(int i = 0; i < vectors.size(); i++) {
+      System.out.println(vectors.get(i));
+    }
+    System.out.println();
 
     faces.add(new Face(vectors));
   }
@@ -268,11 +342,17 @@ public class ConvexHull3D {
   private ArrayList<Face> getNewFaces(ArrayList<Vector3D> vertices,
       Vector3D point) {
     ArrayList<Face> newFaces = new ArrayList<Face>();
+    newF.clear();
     for (int i = 0; i < vertices.size() - 1; i++) {
       newFaces.add(new Face(vertices.get(i), vertices.get(i + 1), point));
+      newF.add(new Face(vertices.get(i), vertices.get(i + 1), point)); // TODO for testing
+
     }
     newFaces.add(new Face(vertices.get(vertices.size() - 1), vertices.get(0),
         point));
+    newF.add(new Face(vertices.get(vertices.size() - 1), vertices.get(0),
+        point)); // TODO for testing
+    
     return newFaces;
   }
 
--- trunk/Java/Development/development/ConvexHull3DTest.java
+++ trunk/Java/Development/development/ConvexHull3DTest.java
@@ -1,88 +1,125 @@
 package development;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Stack;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
+import de.jreality.geometry.CoordinateSystemFactory;
+import de.jreality.geometry.IndexedLineSetFactory;
 import de.jreality.plugin.JRViewer;
 import de.jreality.plugin.basic.ViewShrinkPanelPlugin;
+import de.jreality.scene.Geometry;
 import de.jreality.scene.SceneGraphComponent;
 import de.jreality.tools.RotateTool;
+import de.jreality.util.SceneGraphUtility;
 import de.jtem.jrworkspace.plugin.Controller;
 import de.jtem.jrworkspace.plugin.PluginInfo;
 
 public class ConvexHull3DTest {
-  private static SceneGraphComponent sgc_root;
+  private static SceneGraphComponent sgc_root, sgc_point;
   private static ConvexHull3D hull;
-  private static Stack<ConvexHull3D> done = new Stack<ConvexHull3D>();
-  private static Stack<ConvexHull3D> undone = new Stack<ConvexHull3D>();
+  private static CoordinateSystemFactory factory;
+  // private static Stack<ConvexHull3D> done = new Stack<ConvexHull3D>();
+  // private static Stack<ConvexHull3D> undone = new Stack<ConvexHull3D>();
+  private static Stack<List<SceneGraphComponent>> done = new Stack<List<SceneGraphComponent>>();
+  private static Stack<List<SceneGraphComponent>> undone = new Stack<List<SceneGraphComponent>>();
 
   public static void main(String[] args) {
     ArrayList<Vector3D> list = new ArrayList<Vector3D>();
-//    list.add(new Vector3D(1, 0, 0));
-//    list.add(new Vector3D(0, 1, 0));
-//    list.add(new Vector3D(0, 0, 1));
-//    list.add(new Vector3D(-1, 0, 0));
-//    list.add(new Vector3D(0, -1, 0));
-//    list.add(new Vector3D(0, 0, -.5));
-    
-// dodecahedron    
-    double p = (1+Math.sqrt(5))/2;
+    // list.add(new Vector3D(1, 0, 0));
+    // list.add(new Vector3D(0, 1, 0));
+    // list.add(new Vector3D(0, 0, 1));
+    // list.add(new Vector3D(-1, 0, 0));
+    // list.add(new Vector3D(0, -1, 0));
+    // list.add(new Vector3D(0, 0, -.5));
 
-    list.add(new Vector3D(-1/p, -p, 0));
+    // dodecahedron
+    double p = (1 + Math.sqrt(5)) / 2;
+    // order matters:
+    // list.add(new Vector3D(-1/p, -p, 0));
+    // list.add(new Vector3D(1, -1, -1));
+    // list.add(new Vector3D(-1, -1, 1));
+    // list.add(new Vector3D(1/p, p, 0));
+    //    
+    //        
+    // list.add(new Vector3D(0, -1/p, p));
+    // list.add(new Vector3D(0, -1/p, -p));
+    // list.add(new Vector3D(-1, -1, -1));
+    // list.add(new Vector3D(-1, 1, -1));
+    // list.add(new Vector3D(0, 1/p, p));
+    // list.add(new Vector3D(-1, 1, 1));
+    // list.add(new Vector3D(p, 0, -1/p));
+    // list.add(new Vector3D(0, 1/p, -p));
+    // list.add(new Vector3D(-1/p, p, 0));
+    // list.add(new Vector3D(-p, 0, 1/p));
+    // list.add(new Vector3D(-p, 0, -1/p));
+    // list.add(new Vector3D(1/p, -p, 0));
+    // list.add(new Vector3D(p, 0, 1/p));
+    // list.add(new Vector3D(1, -1, 1));
+    // list.add(new Vector3D(1, 1, -1));
+    // list.add(new Vector3D(1, 1, 1));
+
     list.add(new Vector3D(1, -1, -1));
-    list.add(new Vector3D(-1, -1, 1));
-    list.add(new Vector3D(1/p, p, 0));
-
-    
-    list.add(new Vector3D(0, -1/p, p));
-    list.add(new Vector3D(0, -1/p, -p));
-    list.add(new Vector3D(-1, -1, -1));
-    list.add(new Vector3D(-1, 1, -1));
-    list.add(new Vector3D(0, 1/p, p));
-    list.add(new Vector3D(-1, 1, 1));
-    list.add(new Vector3D(p, 0, -1/p));
-    list.add(new Vector3D(0, 1/p, -p));
-    list.add(new Vector3D(-1/p, p, 0));
-    list.add(new Vector3D(-p, 0, 1/p));
-    list.add(new Vector3D(-p, 0, -1/p));
-    list.add(new Vector3D(1/p, -p, 0));
-    list.add(new Vector3D(p, 0, 1/p));
     list.add(new Vector3D(1, -1, 1));
     list.add(new Vector3D(1, 1, -1));
     list.add(new Vector3D(1, 1, 1));
+    list.add(new Vector3D(-1, -1, 1));
+    list.add(new Vector3D(-1, -1, -1));
+    list.add(new Vector3D(-1, 1, -1));
+    list.add(new Vector3D(-1, 1, 1));
 
-     
+    list.add(new Vector3D(-1 / p, -p, 0));
+    list.add(new Vector3D(1 / p, p, 0));
+    list.add(new Vector3D(-1 / p, p, 0));
+    list.add(new Vector3D(1 / p, -p, 0));
+
+    list.add(new Vector3D(0, -1 / p, p));
+    list.add(new Vector3D(0, -1 / p, -p));
+    list.add(new Vector3D(0, 1 / p, p));
+    list.add(new Vector3D(0, 1 / p, -p));
+
+    list.add(new Vector3D(-p, 0, -1 / p));
+    list.add(new Vector3D(-p, 0, 1 / p));
+    list.add(new Vector3D(p, 0, -1 / p));
+    list.add(new Vector3D(p, 0, 1 / p));
+
     // cube with hat
-//    list.add(new Vector3D(0, 1, 1));
-//    list.add(new Vector3D(1, 1, 0));
-//    list.add(new Vector3D(0, 1, 0));
-//    list.add(new Vector3D(.5, .5, 2));
-//    list.add(new Vector3D(0, 0, 1));
-//    list.add(new Vector3D(0, 0, 0));
-//    list.add(new Vector3D(1, 0, 1));
-//    list.add(new Vector3D(1, 1, 1));
-//    list.add(new Vector3D(1, 0, 0));
-    
-//    Collections.shuffle(list);
+    // list.add(new Vector3D(0, 0, 0));
+    // list.add(new Vector3D(1, 0, 0));
+    // list.add(new Vector3D(0, 0, 1));
+    // list.add(new Vector3D(0, 1, 0));
+    // list.add(new Vector3D(1, 0, 1));
+    // list.add(new Vector3D(1, 1, 1));
+    // list.add(new Vector3D(0, 1, 1));
+    // list.add(new Vector3D(1, 1, 0));
+    // list.add(new Vector3D(.5, .5, 2));
 
-    sgc_root = new SceneGraphComponent();
+ //    Collections.shuffle(list);
+
+    sgc_root = SceneGraphUtility
+    .createFullSceneGraphComponent();
     sgc_root.addTool(new RotateTool());
+    sgc_point = new SceneGraphComponent();
+    sgc_root.addChild(sgc_point);
     hull = new ConvexHull3D(list);
 
     drawHull(hull);
-    done.push(new ConvexHull3D(hull));
+    done.push(copyOf(sgc_root.getChildComponents()));
 
     for (int i = 0; i < hull.getNumberFaces(); i++) {
       SceneGraphComponent sgc = new SceneGraphComponent();
-      sgc.setGeometry(hull.getFaceAt(i).getGeometry());
+      sgc.setGeometry(hull.getFaceAt(i).getGeometry(Color.blue));
       sgc_root.addChild(sgc);
     }
+    
     sgc_root.addTool(new RotateTool());
 
     // set up the main JRViewer
@@ -94,21 +131,95 @@ public class ConvexHull3DTest {
     jrv.startup();
   }
 
-  public static void drawHull(ConvexHull3D hull) {
+  public static void removeChildren(SceneGraphComponent sgc) {
     boolean finished = false;
     while (!finished) {
       try {
-        sgc_root.removeChild(sgc_root.getChildComponent(0));
+        sgc.removeChild(sgc.getChildComponent(0));
       } catch (IndexOutOfBoundsException e) {
         finished = true;
       }
     }
+    sgc.addChild(sgc_point);
+    //setupCoordinates();
+  }
+  
+  private static void setupCoordinates() {
+    factory = new CoordinateSystemFactory(sgc_root);
+    // SET PROPERTIES:
+    double axisScale = 2.0;
+    Font font = new Font("TimesRoman", Font.PLAIN, 80);
+    factory.setAxisScale(axisScale);
+    factory.setLabelScale(0.01);
+    factory.showAxesArrows(true);
+    factory.showLabels(true);
+    factory.setLabelColor(Color.DARK_GRAY);
+    factory.setLabelFont(font);
+    factory.setColor(Color.BLUE);
+
+    // display axes/box/grid
+    factory.showAxes(true);
+    // coords.showBox(true);
+    factory.showGrid(false);
+
+    // beautify box
+    factory.beautify(true);
+  }
+
+  public static void drawHull(ConvexHull3D hull) {
+    removeChildren(sgc_root);
 
     for (int i = 0; i < hull.getNumberFaces(); i++) {
       SceneGraphComponent sgc = new SceneGraphComponent();
-      sgc.setGeometry(hull.getFaceAt(i).getGeometry());
+      sgc.setGeometry(hull.getFaceAt(i).getGeometry(Color.blue));
       sgc_root.addChild(sgc);
     }
+  }
+
+  public static void colorVisible(ConvexHull3D hull) {
+    ArrayList<Face> hiddenFaces = hull.getHiddenFaces();
+    ArrayList<Face> visibleFaces = hull.getVisibleFaces();
+
+    removeChildren(sgc_root);
+
+    for (int i = 0; i < hiddenFaces.size(); i++) {
+      SceneGraphComponent sgc = new SceneGraphComponent();
+      sgc.setGeometry(hiddenFaces.get(i).getGeometry(Color.blue));
+      sgc_root.addChild(sgc);
+    }
+    for (int i = 0; i < visibleFaces.size(); i++) {
+      SceneGraphComponent sgc = new SceneGraphComponent();
+      sgc.setGeometry(visibleFaces.get(i).getGeometry(Color.yellow));
+      sgc_root.addChild(sgc);
+    }
+  }
+
+  public static void colorNewFaces(ConvexHull3D hull) {
+    ArrayList<Face> hiddenFaces = hull.getHiddenFaces();
+    ArrayList<Face> newFaces = hull.getNewFaces();
+
+    removeChildren(sgc_root);
+
+    for (int i = 0; i < hiddenFaces.size(); i++) {
+      SceneGraphComponent sgc = new SceneGraphComponent();
+      sgc.setGeometry(hiddenFaces.get(i).getGeometry(Color.blue));
+      sgc_root.addChild(sgc);
+    }
+    for (int i = 0; i < newFaces.size(); i++) {
+      SceneGraphComponent sgc = new SceneGraphComponent();
+      sgc.setGeometry(newFaces.get(i).getGeometry(Color.red));
+      sgc_root.addChild(sgc);
+    }
+  }
+
+  public static List<SceneGraphComponent> copyOf(List<SceneGraphComponent> list) {
+    ArrayList<SceneGraphComponent> copy = new ArrayList<SceneGraphComponent>();
+    for (int i = 0; i < list.size(); i++) {
+      SceneGraphComponent sgc = new SceneGraphComponent();
+      sgc.setGeometry(list.get(i).getGeometry());
+      copy.add(sgc);
+    }
+    return copy;
   }
 
   // the user interface as a plugin
@@ -116,42 +227,122 @@ public class ConvexHull3DTest {
   // http://www3.math.tu-berlin.de/jreality/api/de/jreality/plugin/basic/ViewShrinkPanelPlugin.html
   // ===========================================================================================
   static class UIPanel_Model extends ViewShrinkPanelPlugin {
+    private enum Steps {
+      drawPoint, colorVisible, addNewFaces, mergeFaces
+    }
+
+    private Steps lastStep = Steps.mergeFaces;
     private JButton forwardButton, backButton;
+    private JButton stepButton;
 
     private void makeUIComponents() {
-
-      class ForwardButtonListener implements ActionListener {
+      class BackButtonListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent arg0) {
-          if (!undone.isEmpty()) {
-            done.push(undone.pop());
-            drawHull(done.peek());
-          } else {
-            if (hull.addPoint()) {
-              drawHull(hull);
-              done.push(new ConvexHull3D(hull));
+          if (!done.firstElement().equals(done.peek())) {
+            System.out.println("undoing");
+            undone.push(done.pop());
+            List<SceneGraphComponent> children = done.peek();
+            System.out.println("children.size() = " + children.size());
+            removeChildren(sgc_root);
+            for (int i = 0; i < children.size(); i++) {
+              sgc_root.addChild(children.get(i));
             }
           }
         }
       }
-      
-      class BackButtonListener implements ActionListener {
+
+      class StepButtonListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent arg0) {
-          if(!done.firstElement().equals(done.peek())) {
-            undone.push(done.pop());
-            drawHull(done.peek());
+          if (!undone.isEmpty()) {
+            System.out.println("redoing");
+            done.push(undone.pop());
+            List<SceneGraphComponent> children = done.peek();
+            removeChildren(sgc_root);
+            for (int i = 0; i < children.size(); i++)
+              sgc_root.addChild(children.get(i));
+          } else {
+            if (lastStep == Steps.mergeFaces) {
+              hull.addPoint();
+              sgc_point.setGeometry(getLineGeometry(hull.lastPoint()));
+              lastStep = Steps.drawPoint;
+              colorVisible(hull);
+              done.push(copyOf(sgc_root.getChildComponents()));
+              lastStep = Steps.colorVisible;
+            } else if (lastStep == Steps.colorVisible) {
+              colorNewFaces(hull);
+              done.push(copyOf(sgc_root.getChildComponents()));
+              lastStep = Steps.addNewFaces;
+            } else if (lastStep == Steps.addNewFaces) {
+              drawHull(hull);
+              done.push(copyOf(sgc_root.getChildComponents()));
+              lastStep = Steps.mergeFaces;
+            }
           }
+        }
+
+        private Geometry getLineGeometry(Vector3D nextPoint) {
+          ArrayList<Vector3D> vertices = hull.getVertices();
+          vertices.add(0, nextPoint);
+          double[][] ilsf_verts = new double[vertices.size()][3];
+          int[][] ilsf_edges = new int[vertices.size() - 1][2];
+
+          for (int i = 0; i < vertices.size(); i++) {
+            ilsf_verts[i] = vertices.get(i).getVectorAsArray();
+            if (i > 0)
+              ilsf_edges[i - 1] = new int[] { 0, i };
+          }
+
+          IndexedLineSetFactory ilsf = new IndexedLineSetFactory();
+          ilsf.setVertexCount(ilsf_verts.length);
+          ilsf.setVertexCoordinates(ilsf_verts);
+          ilsf.setEdgeCount(ilsf_edges.length);
+          ilsf.setEdgeIndices(ilsf_edges);
+
+          ilsf.update();
+          return ilsf.getGeometry();
         }
       }
 
-      forwardButton = new JButton("forward");
-      forwardButton.addActionListener(new ForwardButtonListener());
-      shrinkPanel.add(forwardButton);
-      
+      // class ForwardButtonListener implements ActionListener {
+      // @Override
+      // public void actionPerformed(ActionEvent arg0) {
+      // if (!undone.isEmpty()) {
+      // done.push(undone.pop());
+      // drawHull(done.peek());
+      // } else {
+      // if (hull.addPoint()) {
+      // drawHull(hull);
+      // done.push(new ConvexHull3D(hull));
+      // }
+      // }
+      // }
+      // }
+      //      
+      // class BackButtonListener implements ActionListener {
+      // @Override
+      // public void actionPerformed(ActionEvent arg0) {
+      // if(!done.firstElement().equals(done.peek())) {
+      // undone.push(done.pop());
+      // drawHull(done.peek());
+      // }
+      // }
+      // }
+      //
+      // forwardButton = new JButton("forward");
+      // forwardButton.addActionListener(new ForwardButtonListener());
+      // shrinkPanel.add(forwardButton);
+      //      
+      // backButton = new JButton("back");
+      // backButton.addActionListener(new BackButtonListener());
+      // shrinkPanel.add(backButton);
       backButton = new JButton("back");
       backButton.addActionListener(new BackButtonListener());
       shrinkPanel.add(backButton);
+      stepButton = new JButton("step");
+      stepButton.addActionListener(new StepButtonListener());
+      shrinkPanel.add(stepButton);
 
       shrinkPanel.setBorder(BorderFactory.createEmptyBorder(6, 6, 6, 6));
     }
