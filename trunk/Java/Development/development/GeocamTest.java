ae09461ffa423c52cd0d1d2f2b4ebfd65e9eec56
ahenniges <ahenniges@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2010-07-26 18:38:36 +0000

    Renamed the geoquant access function from At to at.

--- trunk/Java/Development/development/GeocamTest.java
+++ trunk/Java/Development/development/GeocamTest.java
@@ -22,7 +22,7 @@ public class GeocamTest {
     while(i.hasNext()){
       Integer key = (Integer)i.next();
       Edge e = Triangulation.edgeTable.get(key);
-      Length.At(e).setValue(2+Math.random()); //random return value is in [0,1)
+      Length.at(e).setValue(2+Math.random()); //random return value is in [0,1)
     }
     
     //print some tetra info
