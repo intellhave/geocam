d29888e0473fe0763986b6d49d6a19d8e01f1f3a
tdm@math.arizona.edu <tdm@math.arizona.edu@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2010-07-27 18:49:33 +0000

    Added options panel for devapp2d, started on devapp3d

--- trunk/Java/Development/development/CoordTrans3D.java
+++ trunk/Java/Development/development/CoordTrans3D.java
@@ -3,9 +3,11 @@ package development;
 import geoquant.*;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 import triangulation.Tetra;
+import triangulation.Face;
 import triangulation.Vertex;
 
 //note that the coordinates this geoquant gives do not take orientation into account
@@ -14,41 +16,48 @@ public class CoordTrans3D extends Geoquant {
   // Index map
   private static HashMap<TriPosition, CoordTrans3D> Index = new HashMap<TriPosition, CoordTrans3D>();
 
-  //the common face between tetras t1 and t2 has vertices v1, v2, v3
-  //this gives matrix moving tetra t1 onto its common face with t2
-  //to get the orientation right, we need the non-common vertices, w1 and w2
+  //geoquant which gives the affine transformation gluing the tetra t
+  //to the tetra t2 incident to t along the face f
+  //i.e., "change of coordinates" from t to t2
+  
+  //cvitj are coordinates for vertex vi on tetra tj; e.g. cv1t1 -> cv1t2 under 
+  //the returned affine transformation.  cw1 is the non-common vertex in t, cw2 in t2.
   private Coord3D cv1t1,cv2t1,cv3t1,cw1;
   private Coord3D cv1t2,cv2t2,cv3t2,cw2;
 
   private AffineTransformation affineTrans; 
   
-  public CoordTrans3D(Tetra t1, Tetra t2) {
-    super(t1,t2);
-
-    //get list of common vertices
-    LinkedList<Vertex> vertscommon = new LinkedList<Vertex>(t1.getLocalVertices());
-    vertscommon.retainAll(t2.getLocalVertices());
-    Vertex v1 = vertscommon.get(0);
-    Vertex v2 = vertscommon.get(1);
-    Vertex v3 = vertscommon.get(2);
+  public CoordTrans3D(Tetra t, Face f) {
+    super(t,f);
+    
+    //get tetra incident to t along face f
+    Iterator<Tetra> ti = f.getLocalTetras().iterator();
+    Tetra t2 = ti.next();
+    if(t2 == t){ t2 = ti.next(); }
+    
+    //get shared vertices of t and t2
+    Iterator<Vertex> vi = f.getLocalVertices().iterator();
+    Vertex v1 = vi.next();
+    Vertex v2 = vi.next();
+    Vertex v3 = vi.next();
     
     //get non-common vertex on tetra 1
-    LinkedList<Vertex> leftover1 = new LinkedList<Vertex>(t1.getLocalVertices());
-    leftover1.removeAll(vertscommon);
+    LinkedList<Vertex> leftover1 = new LinkedList<Vertex>(t.getLocalVertices());
+    leftover1.removeAll(f.getLocalVertices());
     Vertex w1 = leftover1.get(0);
     
     //get non-common vertex on tetra 2
     LinkedList<Vertex> leftover2 = new LinkedList<Vertex>(t2.getLocalVertices());
-    leftover2.removeAll(vertscommon);
+    leftover2.removeAll(f.getLocalVertices());
     Vertex w2 = leftover2.get(0);
     
-    cv1t1 = Coord3D.At(v1, t1);
-    cv2t1 = Coord3D.At(v2, t1);
-    cv3t1 = Coord3D.At(v3, t1);
+    cv1t1 = Coord3D.At(v1, t);
+    cv2t1 = Coord3D.At(v2, t);
+    cv3t1 = Coord3D.At(v3, t);
     cv1t2 = Coord3D.At(v1, t2);
     cv2t2 = Coord3D.At(v2, t2);
     cv3t2 = Coord3D.At(v3, t2);
-    cw1 = Coord3D.At(w1, t1);
+    cw1 = Coord3D.At(w1, t);
     cw2 = Coord3D.At(w2, t2);
     
     cv1t1.addObserver(this);
@@ -88,19 +97,19 @@ public class CoordTrans3D extends Geoquant {
     Index.remove(pos);
   }
   
-  public static CoordTrans3D At(Tetra t1, Tetra t2) {
-    TriPosition T = new TriPosition(t1.getSerialNumber(), t2.getSerialNumber());
+  public static CoordTrans3D At(Tetra t, Face f) {
+    TriPosition T = new TriPosition(t.getSerialNumber(), f.getSerialNumber());
     CoordTrans3D q = Index.get(T);
     if(q == null) {
-      q = new CoordTrans3D(t1, t2);
+      q = new CoordTrans3D(t, f);
       q.pos = T;
       Index.put(T, q);
     }
     return q;
   }
   
-  public static double valueAt(Tetra t1, Tetra t2) {
-    return At(t1, t2).getValue();
+  public static double valueAt(Tetra t, Face f) {
+    return At(t, f).getValue();
   }
   
   //like getValue(), but returns affine transformation
@@ -109,8 +118,8 @@ public class CoordTrans3D extends Geoquant {
     return affineTrans; 
   }
   //like valueAt(), but returns affine transformation
-  public static AffineTransformation affineTransAt(Tetra t1, Tetra t2) {
-    return At(t1,t2).getAffineTrans();
+  public static AffineTransformation affineTransAt(Tetra t, Face f) {
+    return At(t,f).getAffineTrans();
   }
   
 }
