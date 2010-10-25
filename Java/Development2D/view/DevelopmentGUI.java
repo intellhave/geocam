982698ba6d5abc01631985b32b1d2eb608b3b9a5
snaffle.byte <snaffle.byte@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2010-10-25 05:24:05 +0000

    Added rotation to new Development2D

--- Java/Development2D/view/DevelopmentGUI.java
+++ Java/Development2D/view/DevelopmentGUI.java
@@ -5,6 +5,8 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Iterator;
 
 import javax.swing.JButton;
@@ -12,6 +14,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.Timer;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -23,111 +26,167 @@ import development.Coord2D;
 import development.EmbeddedTriangulation;
 import development.Vector;
 
-public class DevelopmentGUI extends JFrame {
-	/**
+public class DevelopmentGUI extends JFrame implements KeyListener {
+  /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	public static void main(String[] args) {
-		JFrame window = new DevelopmentGUI();
-		window.setVisible(true);
-	}
+  public static void main(String[] args) {
+    JFrame window = new DevelopmentGUI();
+    window.setVisible(true);
+  }
 
-	private int maxDepth = 25;
-	private int currentDepth = maxDepth;
+  private int maxDepth = 25;
+  private int currentDepth = maxDepth;
+  private static final double ROTATION_ANGLE = Math.PI / 90;
 
-	private static Development development;
-	private static Vector sourcePoint;
-	private static Face sourceFace;
-	private static ColorScheme colorScheme;
-	private static DevelopmentView2D view2D;
-	private static DevelopmentView3D view3D;
+  private static Development development;
+  private static Vector sourcePoint;
+  private static Face sourceFace;
+  private static ColorScheme colorScheme;
+  private static DevelopmentView2D view2D;
+  private static DevelopmentView3D view3D;
 
-	private JPanel sliderPanel;
-	private JSlider depthSlider;
-	private JLabel depthLabel = new JLabel("Max Recursion Depth ("
-			+ currentDepth + ")");
-	private JPanel colorPanel;
+  private JPanel sliderPanel;
+  private JSlider depthSlider;
+  private JLabel depthLabel = new JLabel("Max Recursion Depth (" + currentDepth
+      + ")");
+  private JPanel colorPanel;
 
-	public DevelopmentGUI() {
-		EmbeddedTriangulation.readEmbeddedSurface("models/cone.off");
+  // Movement stuff
+  private Timer timer;
+  private boolean moving = false;
 
-		Iterator<Integer> i = null;
-		// pick some arbitrary face and source point
-		i = Triangulation.faceTable.keySet().iterator();
-		sourceFace = Triangulation.faceTable.get(i.next());
+  private enum movements {
+    left, right, forward, back
+  };
 
-		sourcePoint = new Vector(0, 0);
-		Iterator<Vertex> iv = sourceFace.getLocalVertices().iterator();
-		while (iv.hasNext()) {
-			sourcePoint.add(Coord2D.coordAt(iv.next(), sourceFace));
-		}
-		sourcePoint.scale(1.0f / 3.0f);
+  private movements curMovement;
 
-		colorScheme = new ColorScheme(schemes.DEPTH);
-		development = new Development(sourceFace, sourcePoint, maxDepth,
-				currentDepth);
-		view2D = new DevelopmentView2D(development, colorScheme);
-		development.addObserver(view2D);
+  public DevelopmentGUI() {
+    EmbeddedTriangulation.readEmbeddedSurface("models/cone.off");
 
-		view3D = new DevelopmentView3D(development, colorScheme);
-		development.addObserver(view3D);
+    Iterator<Integer> i = null;
+    // pick some arbitrary face and source point
+    i = Triangulation.faceTable.keySet().iterator();
+    sourceFace = Triangulation.faceTable.get(i.next());
 
-		layoutGUI();
-	}
+    sourcePoint = new Vector(0, 0);
+    Iterator<Vertex> iv = sourceFace.getLocalVertices().iterator();
+    while (iv.hasNext()) {
+      sourcePoint.add(Coord2D.coordAt(iv.next(), sourceFace));
+    }
+    sourcePoint.scale(1.0f / 3.0f);
 
-	private void layoutGUI() {
-		this.setSize(220, 200);
-		this.setResizable(true);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		// this.setLayout(new BorderLayout());
+    colorScheme = new ColorScheme(schemes.DEPTH);
+    development = new Development(sourceFace, sourcePoint, maxDepth,
+        currentDepth);
+    view2D = new DevelopmentView2D(development, colorScheme);
+    development.addObserver(view2D);
 
-		sliderPanel = new JPanel();
-		sliderPanel.setLayout(new GridLayout(2, 1));
-		depthSlider = new JSlider(1, maxDepth, currentDepth);
-		depthSlider.setMaximumSize(new Dimension(400, 100));
-		depthSlider.addChangeListener(new ChangeListener() {
-			public void stateChanged(ChangeEvent e) {
-				int val = ((JSlider) e.getSource()).getValue();
-				currentDepth = val;
-				depthLabel.setText("Recursion Depth (" + currentDepth + ")");
-				development.setDesiredDepth(currentDepth);
-			}
-		});
-		sliderPanel.add(depthLabel);
-		sliderPanel.add(depthSlider);
+    view3D = new DevelopmentView3D(development, colorScheme);
+    development.addObserver(view3D);
 
-		colorPanel = new JPanel();
-		JButton depthSchemeButton = new JButton("Depth");
-		depthSchemeButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (colorScheme.getSchemeType() != schemes.DEPTH) {
-					colorScheme = new ColorScheme(schemes.DEPTH);
-					view2D.setColorScheme(colorScheme);
-					view3D.setColorScheme(colorScheme);
-				}
-			}
-		});
-		JButton faceSchemeButton = new JButton("Face");
-		faceSchemeButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (colorScheme.getSchemeType() != schemes.FACE) {
-					colorScheme = new ColorScheme(schemes.FACE);
-					view2D.setColorScheme(colorScheme);
-					view3D.setColorScheme(colorScheme);
-				}
-			}
-		});
-		colorPanel.setLayout(new GridLayout(3, 1));
-		;
-		colorPanel.add(new JLabel("Set Color Scheme"));
-		colorPanel.add(depthSchemeButton);
-		colorPanel.add(faceSchemeButton);
+    timer = new Timer(50, null);
+    timer.addActionListener(new Moving());
 
-		this.setLayout(new FlowLayout());
-		this.add(sliderPanel);
-		this.add(colorPanel);
-	}
+    layoutGUI();
+  }
+
+  private void layoutGUI() {
+    this.setSize(220, 200);
+    this.setResizable(true);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    // this.setLayout(new BorderLayout());
+
+    sliderPanel = new JPanel();
+    sliderPanel.setLayout(new GridLayout(2, 1));
+    depthSlider = new JSlider(1, maxDepth, currentDepth);
+    depthSlider.setMaximumSize(new Dimension(400, 100));
+    depthSlider.addChangeListener(new ChangeListener() {
+      public void stateChanged(ChangeEvent e) {
+        int val = ((JSlider) e.getSource()).getValue();
+        currentDepth = val;
+        depthLabel.setText("Recursion Depth (" + currentDepth + ")");
+        development.setDesiredDepth(currentDepth);
+      }
+    });
+    sliderPanel.add(depthLabel);
+    sliderPanel.add(depthSlider);
+
+    colorPanel = new JPanel();
+    JButton depthSchemeButton = new JButton("Depth");
+    depthSchemeButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        if (colorScheme.getSchemeType() != schemes.DEPTH) {
+          colorScheme = new ColorScheme(schemes.DEPTH);
+          view2D.setColorScheme(colorScheme);
+          view3D.setColorScheme(colorScheme);
+        }
+      }
+    });
+    JButton faceSchemeButton = new JButton("Face");
+    faceSchemeButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        if (colorScheme.getSchemeType() != schemes.FACE) {
+          colorScheme = new ColorScheme(schemes.FACE);
+          view2D.setColorScheme(colorScheme);
+          view3D.setColorScheme(colorScheme);
+        }
+      }
+    });
+    colorPanel.setLayout(new GridLayout(3, 1));
+    ;
+    colorPanel.add(new JLabel("Set Color Scheme"));
+    colorPanel.add(depthSchemeButton);
+    colorPanel.add(faceSchemeButton);
+
+    this.setLayout(new FlowLayout());
+    this.add(sliderPanel);
+    this.add(colorPanel);
+    this.addKeyListener(this);
+    this.setFocusable(true);
+    this.requestFocus();
+  }
+
+  public class Moving implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+      if (curMovement == movements.right) {
+        view2D.rotate(ROTATION_ANGLE);
+        view3D.rotate(ROTATION_ANGLE);
+      } else if (curMovement == movements.left) {
+        view2D.rotate(-ROTATION_ANGLE);
+        view3D.rotate(-ROTATION_ANGLE);
+      }
+    }
+  }
+
+  @Override
+  public void keyTyped(KeyEvent e) {}
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+    if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+      curMovement = movements.right;
+    else if (e.getKeyCode() == KeyEvent.VK_LEFT)
+      curMovement = movements.left;
+    else if (e.getKeyCode() == KeyEvent.VK_UP)
+      curMovement = movements.forward;
+    else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+      curMovement = movements.back;
+    else
+      return;
+    if (moving == false) {
+      moving = true;
+      timer.start();
+    } else {
+      moving = false;
+      timer.stop();
+  }
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {}
 
 }
