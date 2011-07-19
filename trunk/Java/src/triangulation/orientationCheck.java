4ea24e6121db9a33aee3bfc55118059443262367
glickenstein <glickenstein@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2011-07-19 21:55:57 +0000

    Cleaned up. Deleted deprecated Development.

---
+++ trunk/Java/src/triangulation/orientationCheck.java
@@ -0,0 +1,13 @@
+package triangulation;
+
+public class orientationCheck {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
