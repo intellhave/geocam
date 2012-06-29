4fbe444aadc6491d9dab044c13d7287c0f391f2f
jthomas@email.arizona.edu <jthomas@email.arizona.edu@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2012-06-29 00:22:29 +0000

    Updated texture library implementation.

--- trunk/Java/Development2D/viewMKII/TextureLibrary.java
+++ trunk/Java/Development2D/viewMKII/TextureLibrary.java
@@ -4,6 +4,7 @@ import static de.jreality.shader.CommonAttributes.POLYGON_SHADER;
 
 import java.awt.Color;
 import java.io.File;
+import java.util.EnumMap;
 
 import de.jreality.math.MatrixBuilder;
 import de.jreality.scene.Appearance;
@@ -17,43 +18,94 @@ import de.jreality.util.Input;
 
 public class TextureLibrary {
 
-  public enum TextureDescriptor { Dots, Checkerboard }
+  public enum TextureDescriptor {
+    BATHROOMTILE, CHECKER, CLAY, COBBLESTONE, DOTS, GRID, LIGHTHOUSE, PLAID, STUCCO, SWIRLS, ZIGZAG
+  }
+
+  private static EnumMap<TextureDescriptor, Appearance> library;
   
-  static void initializeAppearance(Appearance app, TextureDescriptor td, Color c){
-    DefaultGeometryShader dgs = (DefaultGeometryShader) ShaderUtility.createDefaultGeometryShader(app, true);
+  /*********************************************************************************
+   * This block of code initializes our library of textures, so we aren't always
+   * fetching copies from the disk.
+   *********************************************************************************/
+  static {
+    library = new EnumMap<TextureDescriptor, Appearance>(TextureDescriptor.class);
+    
+    for( TextureDescriptor td : TextureDescriptor.values() ){
+      Appearance tdApp = initializeAppearance( td );
+      library.put(td, tdApp);      
+    }
+  }
+
+  private static Appearance initializeAppearance(TextureDescriptor td) {
+    Appearance app = new Appearance();
+    DefaultGeometryShader dgs = (DefaultGeometryShader) ShaderUtility
+        .createDefaultGeometryShader(app, true);
     dgs.setShowLines(false);
     dgs.setShowPoints(false);
-    DefaultPolygonShader dps = (DefaultPolygonShader) dgs.createPolygonShader("default");
-    
-    dps.setAmbientColor(c);
-    dps.setDiffuseColor(c);
-    dps.setAmbientCoefficient(0.3); // These coefficients seem to help the texture look "bright"
+    DefaultPolygonShader dps = (DefaultPolygonShader) dgs
+        .createPolygonShader("default");
+
+    dps.setAmbientColor(Color.white); // dps.setAmbientColor(c);
+    dps.setAmbientColor(Color.white); // dps.setDiffuseColor(c);
+    dps.setAmbientCoefficient(0.3); // These coefficients seem to help the
+                                    // texture look "bright"
     dps.setDiffuseCoefficient(0.8); // when it gets mapped to the surface.
-    
+
     ImageData id = null;
-    switch(td){
-    case Dots:
-      try{
-        File ff = new File("Data/textures/GridSmall.jpg");
-        id = ImageData.load(Input.getInput(ff));
-      } catch (Exception ee){
-        ee.printStackTrace();
-        System.exit(1);
+
+    try {
+      File ff = null;
+      switch (td) {
+      case BATHROOMTILE:
+        ff = new File("Data/textures/bathroomtile.jpg");
+        break;
+      case CHECKER:
+        ff = new File("Data/textures/checker.jpg");
+        break;
+      case CLAY:
+        ff = new File("Data/textures/clay.jpg");
+        break;
+      case COBBLESTONE:
+        ff = new File("Data/textures/cobblestone.jpg");
+        break;
+      case DOTS:
+        ff = new File("Data/textures/dots.jpg");
+        break;
+      case GRID:
+        ff = new File("Data/textures/grid.jpg");
+        break;
+      case LIGHTHOUSE:
+        ff = new File("Data/textures/lighthouse.jpg");
+        break;
+      case PLAID:
+        ff = new File("Data/textures/plaid.jpg");
+        break;
+      case STUCCO:
+        ff = new File("Data/textures/stucco.jpg");
+        break;
+      case SWIRLS:
+        ff = new File("Data/textures/swirls.jpg");
+        break;
+      case ZIGZAG:
+        ff = new File("Data/textures/zigzag.jpg");
+        break;
       }
-      break;
-    case Checkerboard:
-      try{
-        File ff = new File("Data/textures/checker.gif");
-        id = ImageData.load(Input.getInput(ff));
-      } catch (Exception ee){
-        ee.printStackTrace();
-        System.exit(1);
-      }
-      break;
+
+      id = ImageData.load(Input.getInput(ff));
+    } catch (Exception ee) {
+      System.err.println("Error: Unable to load texture " + td);
+      ee.printStackTrace();
     }
+
     Texture2D tex = TextureUtility.createTexture(app, POLYGON_SHADER, id);
-    tex.setTextureMatrix(MatrixBuilder.euclidean().scale(3.0).getMatrix());
+    tex.setTextureMatrix(MatrixBuilder.euclidean().scale(0.5).getMatrix());
+    
+    return app;
   }
-  
+
+  public static Appearance getAppearance( TextureDescriptor td ){
+    return library.get(td);
+  }
   
 }
