d97237690f4d84ad68a7a8012036b619c26d5b80
smiths949@email.arizona.edu <smiths949@email.arizona.edu@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2012-07-31 18:01:17 +0000

    Added more unfolding code

--- Java/Development2D/view/UnfoldingView.java
+++ Java/Development2D/view/UnfoldingView.java
@@ -1,68 +1,84 @@
 package view;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import marker.Marker;
+import triangulation.Edge;
+import triangulation.Face;
+import triangulation.FaceGrouping;
+import triangulation.StdFace;
+import triangulation.Triangulation;
+import triangulation.Vertex;
+import view.TextureLibrary.TextureDescriptor;
 import de.jreality.geometry.IndexedFaceSetFactory;
 import de.jreality.math.Matrix;
 import de.jreality.math.MatrixBuilder;
 import de.jreality.scene.Appearance;
-import de.jreality.scene.Camera;
 import de.jreality.scene.DirectionalLight;
 import de.jreality.scene.SceneGraphComponent;
 import de.jreality.scene.data.Attribute;
+import development.DevelopmentComputations;
 import development.EmbeddedTriangulation;
 import development.ManifoldPosition;
 import development.TextureCoords;
 import development.Vector;
-import triangulation.Face;
-import triangulation.FaceGrouping;
-import triangulation.Triangulation;
-import view.TextureLibrary.TextureDescriptor;
-import marker.Marker;
 
 public class UnfoldingView extends View {
 
   private Marker source;
   private SceneGraphComponent sourceSGC;
+  private HashMap<Face, SceneGraphComponent> faceGroups;
+  private List<FaceGrouping> checked;
+  private List<SceneGraphComponent> unfolded;
+  private int recursionDepth = 2;
 
   public UnfoldingView(Marker source, FaceAppearanceScheme fas) {
     super(null, null, fas);
     this.source = source;
-   sourceSGC = source.getAppearance().makeSceneGraphComponent();
-   sgcMarkers.addChild(sourceSGC);
-   double[][] light_psns = { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 },
-       { -1, 0, 0 }, { 0, -1, 0 }, { 0, 0, -1 } };
+    sourceSGC = source.getAppearance().makeSceneGraphComponent();
+    sgcMarkers.addChild(sourceSGC);
 
-   Vector origin = new Vector(0, 0, 0);
-   for (double[] psn : light_psns) {
-     SceneGraphComponent sgcLight = new SceneGraphComponent();
-     DirectionalLight light = new DirectionalLight();
-     light.setIntensity(1.0);
-     sgcLight.setLight(light);
+    faceGroups = new HashMap<Face, SceneGraphComponent>();
+    checked = new ArrayList<FaceGrouping>();
+    unfolded = new ArrayList<SceneGraphComponent>();
 
-     MatrixBuilder m = View.lookAt(new Vector(psn), origin);
-     m.assignTo(sgcLight);
+    double[][] light_psns = { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 },
+        { -1, 0, 0 }, { 0, -1, 0 }, { 0, 0, -1 } };
 
-     sgcDevelopment.addChild(sgcLight);
-   }
-    
+    Vector origin = new Vector(0, 0, 0);
+    for (double[] psn : light_psns) {
+      SceneGraphComponent sgcLight = new SceneGraphComponent();
+      DirectionalLight light = new DirectionalLight();
+      light.setIntensity(1.0);
+      sgcLight.setLight(light);
+
+      MatrixBuilder m = View.lookAt(new Vector(psn), origin);
+      m.assignTo(sgcLight);
+
+      sgcDevelopment.addChild(sgcLight);
+    }
   }
 
   public void initializeNewManifold() {
-    Face f = source.getPosition().getFace();
-    SceneGraphComponent sgc = makeSGC(f);
-    sgcDevelopment.addChild(sgc);
-    FaceGrouping fg = Triangulation.groupTable.get(f);
-    List<Face> connectedFaces = fg.getFaces();
-    System.out.println("Number of faces in faceGrouping is: "+connectedFaces.size());
-    for(Face ff: connectedFaces){
-      if(ff!=f)
-        sgc.addChild(makeSGC(ff));
+    for (FaceGrouping fg : Triangulation.groupTable.values()) {
+      if (checked.contains(fg))
+        continue;
+
+      checked.add(fg);
+      SceneGraphComponent group = new SceneGraphComponent();
+      sgcDevelopment.addChild(group);
+
+      List<Face> connectedFaces = fg.getFaces();
+      for (Face f : connectedFaces) {
+        group.addChild(makeSGC(f));
+        faceGroups.put(f, group);
+      }
     }
   }
-  
-  protected SceneGraphComponent makeSGC(Face f){
+
+  protected SceneGraphComponent makeSGC(Face f) {
     double[][] verts = EmbeddedTriangulation.getFaceGeometry(f);
     double[][] texCoords = TextureCoords.getCoordsAsArray(f);
     int[][] ifsf_faces = new int[][] { { 0, 1, 2 } };
@@ -86,17 +102,18 @@ public class UnfoldingView extends View {
     sgc.setAppearance(app);
     sgc.setVisible(true);
     return sgc;
-    
   }
 
   protected void generateMarkerGeometry() {
     Face f = source.getPosition().getFace();
     ManifoldPosition sourcePos = source.getPosition();
     Vector[] tuple = new Vector[4];
-    tuple[0]= EmbeddedTriangulation.getCoord3D(f, sourcePos.getPosition());
-    tuple[1] = EmbeddedTriangulation.embedVector(f,  sourcePos.getDirectionForward());
-    tuple[2] = EmbeddedTriangulation.embedVector(f, sourcePos.getDirectionLeft());
-    tuple[3]= EmbeddedTriangulation.getEmbeddedNormal(f);    
+    tuple[0] = EmbeddedTriangulation.getCoord3D(f, sourcePos.getPosition());
+    tuple[1] = EmbeddedTriangulation.embedVector(f,
+        sourcePos.getDirectionForward());
+    tuple[2] = EmbeddedTriangulation.embedVector(f,
+        sourcePos.getDirectionLeft());
+    tuple[3] = EmbeddedTriangulation.getEmbeddedNormal(f);
     Vector pos = tuple[0];
     Vector forward = Vector.normalize(tuple[1]);
     Vector left = Vector.normalize(tuple[2]);
@@ -129,19 +146,19 @@ public class UnfoldingView extends View {
         .translate(pos.getComponent(0), pos.getComponent(1),
             pos.getComponent(2)).times(matrix)
         .scale(source.getAppearance().getScale()).assignTo(sourceSGC);
-   sourceSGC.setVisible(true);
+    sourceSGC.setVisible(true);
 
   }
 
   protected void generateManifoldGeometry() {
-  generateMarkerGeometry();
 
   }
 
   protected void updateCamera() {
     ManifoldPosition pos = source.getPosition();
-    Vector embedPos = EmbeddedTriangulation.getCoord3D(pos.getFace(), pos.getPosition());
-   
+    Vector embedPos = EmbeddedTriangulation.getCoord3D(pos.getFace(),
+        pos.getPosition());
+
     Vector forward, left, normal;
     forward = EmbeddedTriangulation.embedVector(pos.getFace(),
         pos.getDirectionForward());
@@ -187,10 +204,181 @@ public class UnfoldingView extends View {
     MatrixBuilder.euclidean().translate(adjustedNormal.getVectorAsArray())
         .translate(embedPos.getVectorAsArray()).times(matrix)
         .rotateZ(-Math.PI / 2).assignTo(sgcCamera);
-    
+
   }
 
   public void removeMarker(Marker m) {
+  }
+
+  /*********************************************************************************
+   * displayUnfolding
+   * 
+   * This public method allows AnimationUI to start the unfolding process.
+   *********************************************************************************/
+  public void displayUnfolding() {
+    Face sourceFace = source.getPosition().getFace();
+    unfolded.add(faceGroups.get(sourceFace));
+    Matrix identity = MatrixBuilder.euclidean().getMatrix();
+    unfold(sourceFace, identity, 0);
+  }
+
+  /*********************************************************************************
+   * unfold
+   * 
+   * This recursive method unfolds all faces in the embedding in a systematic way.
+   * 
+   * TODO: add more description here
+   * 
+   * TODO: Make the traversal of the surface "breadth first"
+   * TODO: allow for multiple SceneGraphComponents for each group of faces so that
+   *      they may appear in multiple places
+   * TODO: determine how to clip faces
+   *********************************************************************************/
+  @SuppressWarnings("static-access")
+  private void unfold(Face f, Matrix t, int depth) {
+    if (depth < recursionDepth) {
+      FaceGrouping fg = Triangulation.groupTable.get(f);
+      Face f1 = null;
+      for (Edge sharedEdge : fg.getLocalEdges()) {
+        // determine which face in the group has that edge
+        for (Face face : fg.getFaces()) {
+          if (face.getLocalEdges().contains(sharedEdge)) {
+            f1 = face;
+          }
+        }
+
+        Face f2 = DevelopmentComputations.getNewFace(f1, sharedEdge);
+        SceneGraphComponent rotatingFace = faceGroups.get(f2);
+        
+        //for now, if a face has already been unfolded do not rotate it again
+        if( unfolded.contains(rotatingFace) ) continue;
+        
+        unfolded.add(rotatingFace);
+        
+        double angle = computeAngle(f1, f2);
+        Vertex v1 = sharedEdge.getLocalVertices().get(0);
+        Vertex v2 = sharedEdge.getLocalVertices().get(1);
+        Vertex first = null;
+        Vertex second = null;
+        
+        int orientation = getOrientation(v1, v2, f1);
+        if(orientation == 1) {
+          first = v1;
+          second = v2;
+        }
+        else {
+          first = v2;
+          second = v1;
+        }
+        
+        Vector vec1 = EmbeddedTriangulation.getCoord3D(first);
+        double[] coord1 = new double[3];
+        for (int i = 0; i < 3; i++) {
+          coord1[i] = vec1.getComponent(i);
+        }
+        Vector vec2 = EmbeddedTriangulation.getCoord3D(second);
+        double[] coord2 = new double[3];
+        for (int i = 0; i < 3; i++) {
+          coord2[i] = vec2.getComponent(i);
+        }
+        
+        //perform the transformation on the rotating face
+        //Note that in each step of the animation loop, you must compose the previous
+        //transformation (i.e. the transformation to move the face into position) with 
+        //the current step of the rotation
+        Matrix rot;
+        int step = 0;
+        while (step <= 100) {
+          rot = MatrixBuilder.euclidean().rotate(coord1, coord2, -(step/100.0)*angle).getMatrix();
+          Matrix.times(t, rot).assignTo(rotatingFace);
+          step++;
+          viewer.render();
+          try {
+            Thread.currentThread().sleep(1);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+        rot = MatrixBuilder.euclidean().rotate(coord1, coord2, -angle).getMatrix();
+        Matrix newTrans = Matrix.times(t, rot);
+        
+        unfold(f2, newTrans, depth + 1);
+      }
+    }
+  }
+
+  /*********************************************************************************
+   * computeAngle
+   * 
+   * This method takes two faces in the embedded manifold and returns the angle
+   * through which one of the faces must rotate to lie in the plane of the
+   * other. Note that this is not the same thing as the angle between the faces.
+   * To compute the angle, it finds normal vectors for the faces and returns the
+   * angle between these normal vectors.
+   *********************************************************************************/
+  private double computeAngle(Face f1, Face f2) {
+    // get normal vector of face1
+    List<Vertex> vertices1 = f1.getLocalVertices();
+    Vertex f1vert1 = vertices1.get(0);
+    Vertex f1vert2 = vertices1.get(1);
+    Vertex f1vert3 = vertices1.get(2);
+    Vector f1coord1 = EmbeddedTriangulation.getCoord3D(f1vert1);
+    Vector f1coord2 = EmbeddedTriangulation.getCoord3D(f1vert2);
+    Vector f1coord3 = EmbeddedTriangulation.getCoord3D(f1vert3);
+    Vector f1vec1 = Vector.subtract(f1coord2, f1coord1);
+    Vector f1vec2 = Vector.subtract(f1coord3, f1coord1);
+    Vector f1normal = Vector.cross(f1vec1, f1vec2);
+
+    // get normal vector of face2
+    List<Vertex> vertices2 = f2.getLocalVertices();
+    Vertex f2vert1 = vertices2.get(0);
+    Vertex f2vert2 = vertices2.get(1);
+    Vertex f2vert3 = vertices2.get(2);
+    Vector f2coord1 = EmbeddedTriangulation.getCoord3D(f2vert1);
+    Vector f2coord2 = EmbeddedTriangulation.getCoord3D(f2vert2);
+    Vector f2coord3 = EmbeddedTriangulation.getCoord3D(f2vert3);
+    Vector f2vec1 = Vector.subtract(f2coord2, f2coord1);
+    Vector f2vec2 = Vector.subtract(f2coord3, f2coord1);
+    Vector f2normal = Vector.cross(f2vec1, f2vec2);
+
+    double angle = Math.acos(Vector.dot(f1normal, f2normal) / (f1normal.length() * f2normal.length()));
+
+    return angle;
+  }
+  
+  /*********************************************************************************
+   * getOrientation
+   * 
+   * Given a face and two vertices that define an edge on that face, this method
+   * determines which order these vertices occur in on the face. If the vertices
+   * occur in counter-clockwise order, the method returns 1. If they occur in
+   * clockwise order, it returns -1.
+   * 
+   * TODO: Is this necessary to make the rotations work? And if so, is there a
+   *    simpler algorithm for determining orientation?
+   *********************************************************************************/
+  private int getOrientation(Vertex v1, Vertex v2, Face f) {
+    
+    StdFace standface = new StdFace(f);
+    
+    //Find which vertex on the standard face is equal to v1
+    //If the next vertex on the standard face is v2, the ordering is counter-clockwise
+    //If not, v2 must have occurred directly before v1, and the ordering is clockwise
+    if (standface.v1.equals(v1))
+      if (standface.v2.equals(v2))
+        return 1;
+      else
+        return -1;
+    else if (standface.v2.equals(v1))
+      if (standface.v3.equals(v2))
+        return 1;
+      else
+        return -1;
+    else 
+      if (standface.v1.equals(v2))
+        return 1;
+      else
+        return -1;
   }
 
 }
