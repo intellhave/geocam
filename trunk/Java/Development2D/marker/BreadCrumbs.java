b7fdf9aca9aa494e47d9bd7d3a3e0cd136144eeb
TannerPrynn@gmail.com <TannerPrynn@gmail.com@b7f95403-7d4f-0410-92dd-41f8f3571bcf>
2013-10-10 21:53:00 +0000

    Fix warnings in marker/BreadCrumbs.java for unused imports, add BreadCrumbs.counter() method which returns the current number of crumbs on the trail

--- trunk/Java/Development2D/marker/BreadCrumbs.java
+++ trunk/Java/Development2D/marker/BreadCrumbs.java
@@ -3,7 +3,6 @@ package marker;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-import marker.MarkerAppearance.ModelType;
 import util.Vector;
 import development.ManifoldPosition;
 
@@ -19,47 +18,53 @@ import development.ManifoldPosition;
  *********************************************************************************/
 public class BreadCrumbs {
 
-  private MarkerHandler markers;
-  private Queue<Marker> trail;  
-  private MarkerAppearance crumb = new MarkerAppearance(
-      MarkerAppearance.ModelType.FLAG, .045);
+	private MarkerHandler markers;
+	private Queue<Marker> trail;
+	private MarkerAppearance crumb = new MarkerAppearance(
+			MarkerAppearance.ModelType.FLAG, .045);
 
-  private static int counter = 0;
-  
-  public BreadCrumbs(MarkerHandler mh) {    
-    markers = mh;
-    trail = new ConcurrentLinkedQueue<Marker>();
-  }
+	private int counter = 0;
 
-  /*********************************************************************************
-   * addMarker
-   * 
-   * This method is responsible for adding crumbs to the handler and determining
-   * when crumbs should become invisible. Right now, crumbs are removed once
-   * there are more than 20 markers in the handler. Crumbs are removed according
-   * to when they were placed on the manifold by the user.
-   *********************************************************************************/
-  public void addMarker(ManifoldPosition mp) {
-    ManifoldPosition pos = new ManifoldPosition(mp);
-    Vector vec = new Vector(pos.getDirectionForward());
-    vec.scale(-.25);
-    pos.move(vec);
-    Marker m = new Marker(pos, crumb, Marker.MarkerType.FIXED);
-    trail.add(m);
-    markers.addMarker(m);
-    
-    int numMarkers = trail.size() - 1;
-    if (numMarkers >= 5) {
-      Marker toRemove = trail.poll();
-      toRemove.flagForRemoval();
-    }    
-  }
+	public BreadCrumbs(MarkerHandler mh) {
+		markers = mh;
+		trail = new ConcurrentLinkedQueue<Marker>();
+	}
 
-  public void clear(){
-	  for( Marker m : trail ){
-		  m.setVisible(false);
-		  m.flagForRemoval();
-	  }
-	  trail.clear();
-  }
+	/*********************************************************************************
+	 * addMarker
+	 * 
+	 * This method is responsible for adding crumbs to the handler and
+	 * determining when crumbs should become invisible. Right now, crumbs are
+	 * removed once there are more than 20 markers in the handler. Crumbs are
+	 * removed according to when they were placed on the manifold by the user.
+	 *********************************************************************************/
+	public void addMarker(ManifoldPosition mp) {
+		ManifoldPosition pos = new ManifoldPosition(mp);
+		Vector vec = new Vector(pos.getDirectionForward());
+		vec.scale(-.25);
+		pos.move(vec);
+		Marker m = new Marker(pos, crumb, Marker.MarkerType.FIXED);
+		trail.add(m);
+		markers.addMarker(m);
+		counter++;
+
+		int numMarkers = trail.size() - 1;
+		if (numMarkers >= 5) {
+			Marker toRemove = trail.poll();
+			toRemove.flagForRemoval();
+		}
+	}
+
+	public void clear() {
+		for (Marker m : trail) {
+			m.setVisible(false);
+			m.flagForRemoval();
+		}
+		trail.clear();
+		counter = 0;
+	}
+
+	public int count() {
+		return counter;
+	}
 }
